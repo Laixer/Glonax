049fcd055afecee3839259afc56187729fcb544f
Yorick de Wid <ydewid@gmail.com>
2021-09-14 15:57:38 +0200

    glonax-serial

---
+++ glonax-serial/src/builder.rs
@@ -0,0 +1,225 @@
+use std::{
+    ffi::CString,
+    os::unix::prelude::{OsStrExt, RawFd},
+    path::Path,
+    time::Duration,
+};
+
+use libc::EINVAL;
+use termios::{tcflush, tcsetattr, Termios};
+
+use crate::{BaudRate, CharSize, FlowControl, Parity, StopBits};
+
+pub struct Builder {
+    fd: RawFd,
+    termios: Termios,
+    timeout: Duration,
+    exclusive: bool,
+}
+
+impl Builder {
+    pub fn new(path: &Path) -> super::Result<Self> {
+        use libc::O_RDWR;
+
+        let cstr = match CString::new(path.as_os_str().as_bytes()) {
+            Ok(s) => s,
+            Err(_) => return Err(super::error::from_raw_os_error(EINVAL)),
+        };
+
+        let fd = unsafe { libc::open(cstr.as_ptr(), O_RDWR, 0) }; // O_NONBLOCK
+        if fd < 0 {
+            return Err(super::error::last_os_error());
+        }
+
+        Self::from_fd(fd)
+    }
+
+    fn from_fd(fd: RawFd) -> super::Result<Self> {
+        use libc::{
+            CLOCAL, CREAD, ECHO, ECHOE, ECHOK, ECHONL, ICANON, ICRNL, IEXTEN, IGNBRK, IGNCR, INLCR,
+            ISIG, OPOST, VMIN, VTIME,
+        };
+
+        let mut termios = match Termios::from_fd(fd) {
+            Ok(t) => t,
+            Err(e) => return Err(e.into()),
+        };
+
+        // Setup TTY for binary serial port access.
+        termios.c_iflag &= !(INLCR | IGNCR | ICRNL | IGNBRK);
+        termios.c_oflag &= !OPOST;
+        termios.c_cflag |= CREAD | CLOCAL;
+        termios.c_lflag &= !(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ISIG | IEXTEN);
+
+        termios.c_cc[VMIN] = 0;
+        termios.c_cc[VTIME] = 0;
+
+        Ok(Self {
+            fd,
+            termios,
+            timeout: Duration::from_millis(100),
+            exclusive: true,
+        })
+    }
+
+    #[inline]
+    pub fn set_timeout(mut self, timeout: Duration) -> super::Result<Self> {
+        self.timeout = timeout;
+        Ok(self)
+    }
+
+    #[inline]
+    pub fn set_exclusive(mut self, exclusive: bool) -> Self {
+        self.exclusive = exclusive;
+        self
+    }
+
+    pub fn set_baud_rate(mut self, baud_rate: BaudRate) -> super::Result<Self> {
+        use termios::cfsetspeed;
+        use termios::os::linux::{
+            B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000,
+            B460800, B500000, B576000, B921600,
+        };
+        use termios::os::target::{B115200, B230400, B57600};
+        use termios::{
+            B110, B1200, B134, B150, B1800, B19200, B200, B2400, B300, B38400, B4800, B50, B600,
+            B75, B9600,
+        };
+
+        let baud = match baud_rate {
+            BaudRate::BaudOther(50) => B50,
+            BaudRate::BaudOther(75) => B75,
+            BaudRate::Baud110 => B110,
+            BaudRate::BaudOther(134) => B134,
+            BaudRate::BaudOther(150) => B150,
+            BaudRate::BaudOther(200) => B200,
+            BaudRate::Baud300 => B300,
+            BaudRate::Baud600 => B600,
+            BaudRate::Baud1200 => B1200,
+            BaudRate::BaudOther(1800) => B1800,
+            BaudRate::Baud2400 => B2400,
+            BaudRate::Baud4800 => B4800,
+            BaudRate::Baud9600 => B9600,
+            BaudRate::Baud19200 => B19200,
+            BaudRate::Baud38400 => B38400,
+            BaudRate::Baud57600 => B57600,
+            BaudRate::Baud115200 => B115200,
+            BaudRate::BaudOther(230400) => B230400,
+            BaudRate::BaudOther(460800) => B460800,
+            BaudRate::BaudOther(500000) => B500000,
+            BaudRate::BaudOther(576000) => B576000,
+            BaudRate::BaudOther(921600) => B921600,
+            BaudRate::BaudOther(1000000) => B1000000,
+            BaudRate::BaudOther(1152000) => B1152000,
+            BaudRate::BaudOther(1500000) => B1500000,
+            BaudRate::BaudOther(2000000) => B2000000,
+            BaudRate::BaudOther(2500000) => B2500000,
+            BaudRate::BaudOther(3000000) => B3000000,
+            BaudRate::BaudOther(3500000) => B3500000,
+            BaudRate::BaudOther(4000000) => B4000000,
+            BaudRate::BaudOther(_) => return Err(super::error::from_raw_os_error(EINVAL)),
+        };
+
+        match cfsetspeed(&mut self.termios, baud) {
+            Ok(()) => Ok(self),
+            Err(err) => Err(err.into()),
+        }
+    }
+
+    pub fn set_char_size(mut self, char_size: CharSize) -> Self {
+        use termios::{CS5, CS6, CS7, CS8, CSIZE};
+
+        let size = match char_size {
+            CharSize::Bits5 => CS5,
+            CharSize::Bits6 => CS6,
+            CharSize::Bits7 => CS7,
+            CharSize::Bits8 => CS8,
+        };
+
+        self.termios.c_cflag &= !CSIZE;
+        self.termios.c_cflag |= size;
+
+        self
+    }
+
+    pub fn set_parity(mut self, parity: Parity) -> Self {
+        use termios::{IGNPAR, INPCK, PARENB, PARODD};
+
+        match parity {
+            Parity::ParityNone => {
+                self.termios.c_cflag &= !(PARENB | PARODD);
+                self.termios.c_iflag &= !INPCK;
+                self.termios.c_iflag |= IGNPAR;
+            }
+            Parity::ParityOdd => {
+                self.termios.c_cflag |= PARENB | PARODD;
+                self.termios.c_iflag |= INPCK;
+                self.termios.c_iflag &= !IGNPAR;
+            }
+            Parity::ParityEven => {
+                self.termios.c_cflag &= !PARODD;
+                self.termios.c_cflag |= PARENB;
+                self.termios.c_iflag |= INPCK;
+                self.termios.c_iflag &= !IGNPAR;
+            }
+        };
+
+        self
+    }
+
+    pub fn set_stop_bits(mut self, stop_bits: StopBits) -> Self {
+        use termios::CSTOPB;
+
+        match stop_bits {
+            StopBits::Stop1 => self.termios.c_cflag &= !CSTOPB,
+            StopBits::Stop2 => self.termios.c_cflag |= CSTOPB,
+        };
+
+        self
+    }
+
+    pub fn set_flow_control(mut self, flow_control: FlowControl) -> Self {
+        use termios::os::target::CRTSCTS;
+        use termios::{IXOFF, IXON};
+
+        match flow_control {
+            FlowControl::FlowNone => {
+                self.termios.c_iflag &= !(IXON | IXOFF);
+                self.termios.c_cflag &= !CRTSCTS;
+            }
+            FlowControl::FlowSoftware => {
+                self.termios.c_iflag |= IXON | IXOFF;
+                self.termios.c_cflag &= !CRTSCTS;
+            }
+            FlowControl::FlowHardware => {
+                self.termios.c_iflag &= !(IXON | IXOFF);
+                self.termios.c_cflag |= CRTSCTS;
+            }
+        };
+
+        self
+    }
+
+    pub fn build(self) -> super::Result<crate::Uart> {
+        use libc::{ioctl, TCIOFLUSH, TCSANOW, TIOCEXCL};
+
+        // get exclusive access to device
+        if self.exclusive {
+            let ret = unsafe { ioctl(self.fd, TIOCEXCL) };
+            if ret < 0 {
+                return Err(super::error::last_os_error());
+            }
+        }
+
+        // write settings to TTY
+        if let Err(err) = tcsetattr(self.fd, TCSANOW, &self.termios) {
+            return Err(err.into());
+        }
+
+        if let Err(err) = tcflush(self.fd, TCIOFLUSH) {
+            return Err(err.into());
+        }
+
+        Ok(crate::Uart { fd: self.fd })
+    }
+}
---
+++ glonax-serial/src/lib.rs
@@ -0,0 +1,161 @@
+pub use builder::*;
+pub use uart::*;
+
+mod builder;
+mod error;
+mod uart;
+
+pub use error::{Error, ErrorKind, Result};
+
+/// Serial port baud rates.
+///
+/// ## Portability
+///
+/// The `BaudRate` variants with numeric suffixes, e.g., `Baud9600`, indicate standard baud rates
+/// that are widely-supported on many systems. While non-standard baud rates can be set with
+/// `BaudOther`, their behavior is system-dependent. Some systems may not support arbitrary baud
+/// rates. Using the standard baud rates is more likely to result in portable applications.
+pub enum BaudRate {
+    /// 110 baud.
+    Baud110,
+    /// 300 baud.
+    Baud300,
+    /// 600 baud.
+    Baud600,
+    /// 1200 baud.
+    Baud1200,
+    /// 2400 baud.
+    Baud2400,
+    /// 4800 baud.
+    Baud4800,
+    /// 9600 baud.
+    Baud9600,
+    /// 19,200 baud.
+    Baud19200,
+    /// 38,400 baud.
+    Baud38400,
+    /// 57,600 baud.
+    Baud57600,
+    /// 115,200 baud.
+    Baud115200,
+    /// Non-standard baud rates.
+    ///
+    /// `BaudOther` can be used to set non-standard baud rates by setting its member to be the
+    /// desired baud rate.
+    ///
+    /// ```no_run
+    /// glonax_serial::BaudOther(4_000_000); // 4,000,000 baud
+    /// ```
+    ///
+    /// Non-standard baud rates may not be supported on all systems.
+    BaudOther(usize),
+}
+
+impl BaudRate {
+    /// Creates a `BaudRate` for a particular speed.
+    ///
+    /// This function can be used to select a `BaudRate` variant from an integer containing the
+    /// desired baud rate.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use glonax_serial::BaudRate;
+    /// assert_eq!(BaudRate::Baud9600, BaudRate::from_speed(9600));
+    /// assert_eq!(BaudRate::Baud115200, BaudRate::from_speed(115200));
+    /// assert_eq!(BaudRate::BaudOther(4000000), BaudRate::from_speed(4000000));
+    /// ```
+    pub fn from_speed(speed: usize) -> BaudRate {
+        match speed {
+            110 => BaudRate::Baud110,
+            300 => BaudRate::Baud300,
+            600 => BaudRate::Baud600,
+            1200 => BaudRate::Baud1200,
+            2400 => BaudRate::Baud2400,
+            4800 => BaudRate::Baud4800,
+            9600 => BaudRate::Baud9600,
+            19200 => BaudRate::Baud19200,
+            38400 => BaudRate::Baud38400,
+            57600 => BaudRate::Baud57600,
+            115200 => BaudRate::Baud115200,
+            n => BaudRate::BaudOther(n),
+        }
+    }
+
+    /// Returns the baud rate as an integer.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use glonax_serial::BaudRate;
+    /// assert_eq!(9600, BaudRate::Baud9600.speed());
+    /// assert_eq!(115200, BaudRate::Baud115200.speed());
+    /// assert_eq!(4000000, BaudRate::BaudOther(4000000).speed());
+    /// ```
+    pub fn speed(&self) -> usize {
+        match *self {
+            BaudRate::Baud110 => 110,
+            BaudRate::Baud300 => 300,
+            BaudRate::Baud600 => 600,
+            BaudRate::Baud1200 => 1200,
+            BaudRate::Baud2400 => 2400,
+            BaudRate::Baud4800 => 4800,
+            BaudRate::Baud9600 => 9600,
+            BaudRate::Baud19200 => 19200,
+            BaudRate::Baud38400 => 38400,
+            BaudRate::Baud57600 => 57600,
+            BaudRate::Baud115200 => 115200,
+            BaudRate::BaudOther(n) => n,
+        }
+    }
+}
+
+/// Number of bits per character.
+pub enum CharSize {
+    /// 5 bits per character.
+    Bits5,
+    /// 6 bits per character.
+    Bits6,
+    /// 7 bits per character.
+    Bits7,
+    /// 8 bits per character.
+    Bits8,
+}
+
+/// Parity checking modes.
+///
+/// When parity checking is enabled (`ParityOdd` or `ParityEven`) an extra bit is transmitted with
+/// each character. The value of the parity bit is arranged so that the number of 1 bits in the
+/// character (including the parity bit) is an even number (`ParityEven`) or an odd number
+/// (`ParityOdd`).
+///
+/// Parity checking is disabled by setting `ParityNone`, in which case parity bits are not
+/// transmitted.
+pub enum Parity {
+    /// No parity bit.
+    ParityNone,
+    /// Parity bit sets odd number of 1 bits.
+    ParityOdd,
+    /// Parity bit sets even number of 1 bits.
+    ParityEven,
+}
+
+/// Number of stop bits.
+///
+/// Stop bits are transmitted after every character.
+pub enum StopBits {
+    /// One stop bit.
+    Stop1,
+    /// Two stop bits.
+    Stop2,
+}
+
+/// Flow control modes.
+pub enum FlowControl {
+    /// No flow control.
+    FlowNone,
+    /// Flow control using XON/XOFF bytes.
+    FlowSoftware,
+    /// Flow control using RTS/CTS signals.
+    FlowHardware,
+}
