170597abb981cbca40019ea7aff0612439662a15
Yorick de Wid <ydewid@gmail.com>
2021-10-01 12:06:17 +0200

    Motion profile

--- glonax-runtime/src/kernel/excavator/drive.rs
+++ glonax-runtime/src/kernel/excavator/drive.rs
@@ -10,30 +10,118 @@ use super::Actuator;
 /// This program is part of the excavator kernel. It
 /// drives both tracks straight forward for 5 seconds
 /// then stops.
-pub struct DriveProgram;
+pub struct DriveProgram {
+    profile: TrapezoidalDistanceProfile,
+}
 
-const DRIVE_SPEED_LEFT: i16 = 200;
-const DRIVE_SPEED_RIGHT: i16 = 200;
-const DRIVE_TIME: u64 = 5;
+// const DRIVE_SPEED_MAX_MSEC: f64 = 5_000.0 / 3600.0;
+const DRIVE_SPEED_MAX_MSEC: f32 = 16.1 / 30.0;
+
+const DRIVE_DISTANCE: u64 = 30;
+
+struct TrapezoidalProfile {
+    ramp_time: std::time::Duration,
+    motion_time: std::time::Duration,
+    power_range: (f32, f32),
+}
+
+impl TrapezoidalProfile {
+    fn phase_frame(&self, duration: &std::time::Duration) -> (i32, f32) {
+        let p_delta = (self.power_range.1 - self.power_range.0) / self.ramp_time.as_millis() as f32;
+
+        if duration < &self.ramp_time {
+            let power = self.power_range.0 + duration.as_millis() as f32 * p_delta;
+
+            (0, power)
+        } else if duration > &self.ramp_time && duration < &self.motion_time {
+            (1, self.power_range.1)
+        } else {
+            if let Some(duration_phase2) =
+                duration.checked_sub(self.motion_time.max(self.ramp_time))
+            {
+                if duration_phase2 < self.ramp_time {
+                    let power = self.power_range.1 - duration_phase2.as_millis() as f32 * p_delta;
+                    (2, power)
+                } else {
+                    (2, 0.0)
+                }
+            } else {
+                (2, 0.0)
+            }
+        }
+    }
+
+    fn is_finished(&self, duration: &std::time::Duration) -> bool {
+        let (phase, power) = self.phase_frame(duration);
+        phase == 2 && power == 0.0
+    }
+}
+
+struct TrapezoidalDistanceProfile {
+    inner: TrapezoidalProfile,
+    max_speed: f32,
+}
+
+impl TrapezoidalDistanceProfile {
+    fn new(max_speed: f32, distance: f32) -> Self {
+        Self {
+            inner: TrapezoidalProfile {
+                ramp_time: std::time::Duration::from_secs(3),
+                motion_time: std::time::Duration::from_secs_f32(distance / max_speed),
+                power_range: (175.0, 255.0),
+            },
+            max_speed,
+        }
+    }
+
+    fn phase_frame(&self, duration: &std::time::Duration) -> (i32, f32, f32) {
+        let (phase, power) = self.inner.phase_frame(duration);
+
+        // let d = match phase {
+        //     0 => ((self.max_speed / 255.0) * power) * duration.as_secs_f32(),
+        //     1 => self.max_speed * duration.as_secs_f32(),
+        //     2 => 0.0,
+        //     _ => 0.0,
+        // };
+
+        (phase, power, self.max_speed * duration.as_secs_f32())
+    }
+
+    fn is_finished(&self, duration: &std::time::Duration) -> bool {
+        self.inner.is_finished(duration)
+    }
+}
 
 impl DriveProgram {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            profile: TrapezoidalDistanceProfile::new(DRIVE_SPEED_MAX_MSEC, 19.0),
+        }
     }
 }
 
 impl Program for DriveProgram {
-    fn step(&mut self, _: &mut Context) -> Option<Motion> {
-        Some(Motion::Change(vec![
-            (Actuator::LimpLeft.into(), DRIVE_SPEED_LEFT),
-            (Actuator::LimpRight.into(), DRIVE_SPEED_RIGHT),
-        ]))
+    fn step(&mut self, context: &mut Context) -> Option<Motion> {
+        let (phase, power, distance) = self.profile.phase_frame(&context.start.elapsed());
+
+        info!(
+            "Phase {}; Time: {}; Power: {}; Distance: {}",
+            phase,
+            context.start.elapsed().as_secs(),
+            power.round(),
+            distance.round()
+        );
+
+        // Some(Motion::Change(vec![
+        //     (Actuator::LimpLeft.into(), phase.1.round() as i16),
+        //     (Actuator::LimpRight.into(), phase.1.round() as i16),
+        // ]))
+
+        None
     }
 
     fn can_terminate(&self, context: &mut Context) -> bool {
-        let sec_since_boot = context.start.elapsed().as_secs();
-        info!("Programm running for {} seconds", sec_since_boot);
-        sec_since_boot >= DRIVE_TIME
+        self.profile.is_finished(&context.start.elapsed())
     }
 
     fn term_action(&self, _: &mut Context) -> Option<Motion> {
