b4ffab16b44782e87e6d8fe111a139d07bf04b8d
Yorick de Wid <ydewid@gmail.com>
2021-10-03 13:15:06 +0200

    Daemon configuration file

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,10 +3,12 @@
 # Build directory tree
 mkdir -p ./target/glonax_1.0-1_amd64/DEBIAN
 mkdir -p ./target/glonax_1.0-1_amd64/etc/systemd/system
+mkdir -p ./target/glonax_1.0-1_amd64/etc/glonax
 mkdir -p ./target/glonax_1.0-1_amd64/usr/local/bin
 # Copy files
 cp ./contrib/deb/control ./target/glonax_1.0-1_amd64/DEBIAN
 cp ./contrib/glonaxd.service ./target/glonax_1.0-1_amd64/etc/systemd/system
+cp ./contrib/etc/glonaxd.toml ./target/glonax_1.0-1_amd64/etc/glonax
 cp ./target/release/glonaxd ./target/release/icedump ./target/glonax_1.0-1_amd64/usr/local/bin
 # Build the package
 dpkg-deb --build --root-owner-group ./target/glonax_1.0-1_amd64
