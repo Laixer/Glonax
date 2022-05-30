bc3c2f403b236c55b7cfdaef8891d228e7b7ad20
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-05-30 14:16:15 +0200

    Create sink from node

--- glonax-runtime/src/device/driver/sink.rs
+++ glonax-runtime/src/device/driver/sink.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use glonax_core::motion::Motion;
 
 use crate::{
@@ -23,13 +25,18 @@ impl UserDevice for Sink {
     }
 
     #[inline]
-    async fn from_sysname(name: &str, _config: &Config) -> device::Result<Self> {
-        Ok(Self::new(name))
+    async fn from_sysname(_name: &str, _config: &Config) -> device::Result<Self> {
+        unimplemented!()
+    }
+
+    #[inline]
+    async fn from_node_path(name: &str, _config: &Config, path: &Path) -> device::Result<Self> {
+        Ok(Self::new(name, path))
     }
 }
 
 impl Sink {
-    fn new(name: &str) -> Self {
+    fn new(name: &str, _path: &Path) -> Self {
         Self {
             sysname: name.to_owned(),
         }
@@ -47,19 +54,19 @@ impl MotionDevice for Sink {
     async fn actuate(&mut self, motion: Motion) {
         match motion {
             Motion::StopAll => {
-                trace!("Stop all actuators");
+                trace!("Stop all pseudo actuators");
             }
             Motion::ResumeAll => {
-                trace!("Resume all actuators");
+                trace!("Resume all pseudo actuators");
             }
             Motion::Stop(actuators) => {
                 for actuator in actuators {
-                    trace!("Stop actuator {} ", actuator);
+                    trace!("Stop pseudo actuator {} ", actuator);
                 }
             }
             Motion::Change(actuators) => {
                 for (actuator, value) in actuators {
-                    trace!("Change actuator {} to value {}", actuator, value);
+                    trace!("Change pseudo actuator {} to value {}", actuator, value);
                 }
             }
         }
