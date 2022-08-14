f45eb813daf0311594b82e59505ab2147a609344
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-08-14 10:12:08 -0400

    Repair deb package generation

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,8 +7,8 @@ mkdir -p ./target/glonax_1.0-1_amd64/etc/glonax
 mkdir -p ./target/glonax_1.0-1_amd64/usr/local/bin
 # Copy files
 cp ./contrib/deb/control ./target/glonax_1.0-1_amd64/DEBIAN
-cp ./contrib/glonaxd.service ./target/glonax_1.0-1_amd64/etc/systemd/system
+cp ./contrib/glonax.service ./target/glonax_1.0-1_amd64/etc/systemd/system
 cp ./contrib/etc/glonaxd.toml ./target/glonax_1.0-1_amd64/etc/glonax
-cp ./target/release/glonaxd ./target/release/icedump ./target/glonax_1.0-1_amd64/usr/local/bin
+cp ./target/release/glonaxd ./target/release/glonax-netctl ./target/glonax_1.0-1_amd64/usr/local/bin
 # Build the package
 dpkg-deb --build --root-owner-group ./target/glonax_1.0-1_amd64
