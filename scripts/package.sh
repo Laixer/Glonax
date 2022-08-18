5e1fa8ff583b641509909c27ac6a933ae7a99f32
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-08-18 10:46:48 +0000

    FIX: Workspace

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+set -e
 
 # Build directory tree
 mkdir -p ./target/glonax_1.0-1_amd64/DEBIAN
+mkdir -p ./target/glonax_1.0-1_amd64/etc/udev/rules.d
 mkdir -p ./target/glonax_1.0-1_amd64/etc/systemd/system
 mkdir -p ./target/glonax_1.0-1_amd64/etc/glonax
 mkdir -p ./target/glonax_1.0-1_amd64/usr/local/bin
+
 # Copy files
 cp ./contrib/deb/control ./target/glonax_1.0-1_amd64/DEBIAN
-cp ./contrib/glonax.service ./target/glonax_1.0-1_amd64/etc/systemd/system
+cp ./contrib/udev/79-glonax.rules ./target/glonax_1.0-1_amd64/etc/udev/rules.d
+cp ./contrib/systemd/glonax.service ./target/glonax_1.0-1_amd64/etc/systemd/system
+cp ./contrib/systemd/glonax-inputd@.service ./target/glonax_1.0-1_amd64/etc/systemd/system
 cp ./contrib/etc/glonaxd.toml ./target/glonax_1.0-1_amd64/etc/glonax
-cp ./target/release/glonaxd ./target/release/glonax-netctl ./target/glonax_1.0-1_amd64/usr/local/bin
+cp ./target/release/glonaxd ./target/glonax_1.0-1_amd64/usr/local/bin
+cp ./target/release/glonax-inputd ./target/glonax_1.0-1_amd64/usr/local/bin
+cp ./target/release/glonax-netctl ./target/glonax_1.0-1_amd64/usr/local/bin
+
 # Build the package
 dpkg-deb --build --root-owner-group ./target/glonax_1.0-1_amd64
