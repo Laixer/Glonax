5e28711f5e316f3ce4dd9e87757aa07a04eb9b2d
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-08-27 16:03:51 +0000

    Package deb

--- scripts/package.sh
+++ scripts/package.sh
@@ -10,9 +10,11 @@ mkdir -p ./target/glonax_1.0-1_amd64/usr/local/bin
 # Copy files
 cp ./contrib/deb/control ./target/glonax_1.0-1_amd64/DEBIAN
 cp ./contrib/udev/79-glonax.rules ./target/glonax_1.0-1_amd64/etc/udev/rules.d
-cp ./contrib/systemd/glonax.service ./target/glonax_1.0-1_amd64/etc/systemd/system
+cp ./contrib/systemd/glonax-ecud.service ./target/glonax_1.0-1_amd64/etc/systemd/system
+cp ./contrib/systemd/glonax-execd.service ./target/glonax_1.0-1_amd64/etc/systemd/system
 cp ./contrib/systemd/glonax-inputd@.service ./target/glonax_1.0-1_amd64/etc/systemd/system
-cp ./target/release/glonaxd ./target/glonax_1.0-1_amd64/usr/local/bin
+cp ./target/release/glonax-ecud ./target/glonax_1.0-1_amd64/usr/local/bin
+cp ./target/release/glonax-execd ./target/glonax_1.0-1_amd64/usr/local/bin
 cp ./target/release/glonax-inputd ./target/glonax_1.0-1_amd64/usr/local/bin
 cp ./target/release/glonax-netctl ./target/glonax_1.0-1_amd64/usr/local/bin
 
