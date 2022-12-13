2f273fce4814390bb34439ceb9e0886c618d763f
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-12-13 22:29:54 +0100

    Read values from OBJ

--- glonax-runtime/src/runtime/cli.rs
+++ glonax-runtime/src/runtime/cli.rs
@@ -14,56 +14,81 @@ pub(crate) async fn exec_service<K: Operand>(
         }
     });
 
-    program_manager
-        .publish(ProgramArgument {
-            id: 603,
-            parameters: vec![5.21, 0.0, 0.0],
-        })
-        .await;
-    program_manager
-        .publish(ProgramArgument {
-            id: 603,
-            parameters: vec![3.77, 1.10, 4.07],
-        })
-        .await;
-    program_manager
-        .publish(ProgramArgument {
-            id: 603,
-            parameters: vec![5.28, 1.32, 5.70],
-        })
-        .await;
-    program_manager
-        .publish(ProgramArgument {
-            id: 603,
-            parameters: vec![3.16, -0.45, 3.41],
-        })
-        .await;
-    program_manager
-        .publish(ProgramArgument {
-            id: 603,
-            parameters: vec![3.16, 0.55, 3.41],
-        })
-        .await;
-    program_manager
-        .publish(ProgramArgument {
-            id: 603,
-            parameters: vec![4.45, 0.55, -0.33],
-        })
-        .await;
-    program_manager
-        .publish(ProgramArgument {
-            id: 603,
-            parameters: vec![6.73, 2.35, -4.27],
-        })
-        .await;
-    program_manager
-        .publish(ProgramArgument {
-            id: 603,
-            parameters: vec![5.21, 0.0, 0.0],
-        })
-        .await;
+    let (models, _) = tobj::load_obj(
+        "/home/yorick/Projects/glonax/path.obj",
+        &tobj::LoadOptions::default(),
+    )
+    .expect("ouch");
 
-    info!("Program 901 submitted");
+    for model in &models {
+        let mesh = &model.mesh;
+
+        for v in 0..mesh.positions.len() / 3 {
+            let x = mesh.positions[3 * v];
+            let y = mesh.positions[3 * v + 1];
+            let z = mesh.positions[3 * v + 2];
+
+            let yy = ProgramArgument {
+                id: 603,
+                parameters: vec![x, y, z],
+            };
+
+            program_manager.publish(yy).await;
+        }
+    }
+
+    // dbg!(models);
+
+    // program_manager
+    //     .publish(ProgramArgument {
+    //         id: 603,
+    //         parameters: vec![5.21, 0.0, 0.0],
+    //     })
+    //     .await;
+    // program_manager
+    //     .publish(ProgramArgument {
+    //         id: 603,
+    //         parameters: vec![3.77, 1.10, 4.07],
+    //     })
+    //     .await;
+    // program_manager
+    //     .publish(ProgramArgument {
+    //         id: 603,
+    //         parameters: vec![5.28, 1.32, 5.70],
+    //     })
+    //     .await;
+    // program_manager
+    //     .publish(ProgramArgument {
+    //         id: 603,
+    //         parameters: vec![3.16, -0.45, 3.41],
+    //     })
+    //     .await;
+    // program_manager
+    //     .publish(ProgramArgument {
+    //         id: 603,
+    //         parameters: vec![3.16, 0.55, 3.41],
+    //     })
+    //     .await;
+    // program_manager
+    //     .publish(ProgramArgument {
+    //         id: 603,
+    //         parameters: vec![4.45, 0.55, -0.33],
+    //     })
+    //     .await;
+    // program_manager
+    //     .publish(ProgramArgument {
+    //         id: 603,
+    //         parameters: vec![6.73, 2.35, -4.27],
+    //     })
+    //     .await;
+    // program_manager
+    //     .publish(ProgramArgument {
+    //         id: 603,
+    //         parameters: vec![5.21, 0.0, 0.0],
+    //     })
+    //     .await;
+
+    // info!("Program 901 submitted");
 
     tokio::time::sleep(std::time::Duration::from_millis(100)).await;
 
