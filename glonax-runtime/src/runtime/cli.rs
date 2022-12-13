59bdd92f4ea6f1624b2f9eefbe27d86249e65233
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-12-13 12:33:55 +0100

    Default program

--- glonax-runtime/src/runtime/cli.rs
+++ glonax-runtime/src/runtime/cli.rs
@@ -16,8 +16,50 @@ pub(crate) async fn exec_service<K: Operand>(
 
     program_manager
         .publish(ProgramArgument {
-            id: 900,
-            parameters: vec![10.0],
+            id: 603,
+            parameters: vec![5.21, 0.0, 0.0],
+        })
+        .await;
+    program_manager
+        .publish(ProgramArgument {
+            id: 603,
+            parameters: vec![3.77, 1.10, 4.07],
+        })
+        .await;
+    program_manager
+        .publish(ProgramArgument {
+            id: 603,
+            parameters: vec![5.28, 1.32, 5.70],
+        })
+        .await;
+    program_manager
+        .publish(ProgramArgument {
+            id: 603,
+            parameters: vec![3.16, -0.45, 3.41],
+        })
+        .await;
+    program_manager
+        .publish(ProgramArgument {
+            id: 603,
+            parameters: vec![3.16, 0.55, 3.41],
+        })
+        .await;
+    program_manager
+        .publish(ProgramArgument {
+            id: 603,
+            parameters: vec![4.45, 0.55, -0.33],
+        })
+        .await;
+    program_manager
+        .publish(ProgramArgument {
+            id: 603,
+            parameters: vec![6.73, 2.35, -4.27],
+        })
+        .await;
+    program_manager
+        .publish(ProgramArgument {
+            id: 603,
+            parameters: vec![5.21, 0.0, 0.0],
         })
         .await;
 
