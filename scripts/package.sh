705a55eaa6b52087bf950d07e3b5be71d58a278a
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-02-19 13:57:34 +0100

    Remove applications from build

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION=1.4-1
+VERSION=2.3-1
 
 # Build directory tree
 mkdir -p ./target/glonax_${VERSION}_amd64/DEBIAN
@@ -13,7 +13,6 @@ mkdir -p ./target/glonax_${VERSION}_amd64/usr/local/bin
 cp ./contrib/deb/control ./target/glonax_${VERSION}_amd64/DEBIAN
 cp ./contrib/udev/79-glonax.rules ./target/glonax_${VERSION}_amd64/etc/udev/rules.d
 cp ./contrib/systemd/glonax-ecud@.service ./target/glonax_${VERSION}_amd64/etc/systemd/system
-cp ./contrib/systemd/glonax-execd.service ./target/glonax_${VERSION}_amd64/etc/systemd/system
 cp ./contrib/systemd/glonax-inputd@.service ./target/glonax_${VERSION}_amd64/etc/systemd/system
 cp ./target/release/glonax-ecud ./target/glonax_${VERSION}_amd64/usr/local/bin
 cp ./target/release/glonax-inputd ./target/glonax_${VERSION}_amd64/usr/local/bin
@@ -21,3 +20,6 @@ cp ./target/release/glonax-netctl ./target/glonax_${VERSION}_amd64/usr/local/bin
 
 # Build the package
 dpkg-deb --build --root-owner-group ./target/glonax_${VERSION}_amd64
+
+# Cleanup
+rm -rf ./target/glonax_${VERSION}_amd64
