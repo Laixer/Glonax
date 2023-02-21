188db48e2b7c2ae0612aaf95fdcd7e6ab5844b50
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-02-21 13:13:17 +0100

    Remove encoders

--- glonax-runtime/src/device/mecu.rs
+++ glonax-runtime/src/device/mecu.rs
@@ -4,7 +4,7 @@ use glonax_j1939::{Frame, PGN};
 
 use crate::{
     net::{J1939Network, KueblerEncoderService},
-    signal::{Encoder, SignalPublisher},
+    signal::SignalPublisher,
 };
 
 #[derive(Debug, serde::Serialize)]
@@ -42,28 +42,6 @@ impl crate::net::Routable for Mecu {
 
     fn ingress(&mut self, pgn: PGN, frame: &Frame) -> bool {
         if self.arm_encoder.node() == frame.id().sa() && self.arm_encoder.ingress(pgn, frame) {
-            // /// Arm encoder range.
-            // pub const ARM_ENCODER_RANGE: std::ops::Range<f32> = 24900.0..51600.0;
-            // /// Arm angle range.
-            // pub const ARM_ANGLE_RANGE: std::ops::Range<f32> = 0.0..2.1;
-
-            // let encoder = Encoder::new(ARM_ENCODER_RANGE, ARM_ANGLE_RANGE);
-
-            // let angle = encoder.scale(self.arm_encoder.position() as f32);
-            // let percentage = encoder.scale_to(100.0, self.arm_encoder.position() as f32);
-
-            // write!(
-            //     f,
-            //     "Position: {:>5} {:>6.2}rad {:>6.2}°; Speed {:>5}; State: {}",
-            //     self.position,
-            //     self.position as f32 / 1000.0,
-            //     crate::core::rad_to_deg(self.position as f32 / 1000.0),
-            //     self.speed,
-            //     self.state
-            //         .as_ref()
-            //         .map_or_else(|| "-".to_owned(), |f| f.to_string()),
-            // )
-
             debug!(
                 "Arm Encoder: {:?}\tAngle rel.: {:>+5.2}rad {:>+5.2}°",
                 self.arm_encoder.position(),
@@ -85,16 +63,6 @@ impl crate::net::Routable for Mecu {
         } else if self.boom_encoder.node() == frame.id().sa()
             && self.boom_encoder.ingress(pgn, frame)
         {
-            // /// Boom encoder range.
-            // pub const BOOM_ENCODER_RANGE: std::ops::Range<f32> = 108900.0..195600.0;
-            // /// Boom angle range.
-            // pub const BOOM_ANGLE_RANGE: std::ops::Range<f32> = 0.0..1.178;
-
-            // let encoder = Encoder::new(BOOM_ENCODER_RANGE, BOOM_ANGLE_RANGE);
-
-            // let angle = encoder.scale(self.boom_encoder.position() as f32);
-            // let percentage = encoder.scale_to(100.0, self.boom_encoder.position() as f32);
-
             debug!(
                 "Boom Encoder: {:?}\tAngle rel.: {:>+5.2}rad {:>+5.2}°",
                 self.boom_encoder.position(),
@@ -107,7 +75,7 @@ impl crate::net::Routable for Mecu {
                 EncoderSet {
                     position: self.boom_encoder.position(),
                     speed: self.boom_encoder.speed(),
-                    angle: self.boom_encoder.speed() as f32 / 1000.0,
+                    angle: self.boom_encoder.position() as f32 / 1000.0,
                     percentage: 0.0,
                 },
             );
@@ -116,16 +84,6 @@ impl crate::net::Routable for Mecu {
         } else if self.frame_encoder.node() == frame.id().sa()
             && self.frame_encoder.ingress(pgn, frame)
         {
-            // / Slew encoder range.
-            // pub const SLEW_ENCODER_RANGE: std::ops::Range<f32> = 0.0..290000.0;
-            // / Slew angle range.
-            // pub const SLEW_ANGLE_RANGE: std::ops::Range<f32> = 0.0..core::f32::consts::PI * 2.0;
-
-            // let encoder = Encoder::new(SLEW_ENCODER_RANGE, SLEW_ANGLE_RANGE);
-
-            // let angle = encoder.scale(self.frame_encoder.position() as f32);
-            // let percentage = encoder.scale_to(100.0, self.frame_encoder.position() as f32);
-
             debug!(
                 "Frame Encoder: {:?}\tAngle rel.: {:>+5.2}rad {:>+5.2}°",
                 self.frame_encoder.position(),
@@ -147,16 +105,6 @@ impl crate::net::Routable for Mecu {
         } else if self.attachment_encoder.node() == frame.id().sa()
             && self.attachment_encoder.ingress(pgn, frame)
         {
-            // /// Slew encoder range.
-            // pub const SLEW_ENCODER_RANGE: std::ops::Range<f32> = 0.0..290000.0;
-            // /// Slew angle range.
-            // pub const SLEW_ANGLE_RANGE: std::ops::Range<f32> = 0.0..core::f32::consts::PI * 2.0;
-
-            // let encoder = Encoder::new(SLEW_ENCODER_RANGE, SLEW_ANGLE_RANGE);
-
-            // let angle = encoder.scale(self.turn_encoder.position() as f32);
-            // let percentage = encoder.scale_to(100.0, self.turn_encoder.position() as f32);
-
             debug!(
                 "Attachment Encoder: {:?}\tAngle rel.: {:>+5.2}rad {:>+5.2}°",
                 self.attachment_encoder.position(),
