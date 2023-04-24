73920133613f3251826300e7124bd7749c34d8ac
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-04-24 18:46:27 +0200

    Rename to trace daemon

--- scripts/package.sh
+++ scripts/package.sh
@@ -15,12 +15,13 @@ cp ./contrib/deb/postinst ./target/glonax_${VERSION}_amd64/DEBIAN
 cp ./contrib/udev/79-glonax.rules ./target/glonax_${VERSION}_amd64/etc/udev/rules.d
 cp ./contrib/systemd/glonax-ecud@.service ./target/glonax_${VERSION}_amd64/etc/systemd/system
 cp ./contrib/systemd/glonax-inputd.service ./target/glonax_${VERSION}_amd64/etc/systemd/system
+cp ./contrib/systemd/glonax-traced@.service ./target/glonax_${VERSION}_amd64/etc/systemd/system
 
 # Copy binaries
 cp ./target/release/glonax-ecud ./target/glonax_${VERSION}_amd64/usr/local/bin
 cp ./target/release/glonax-inputd ./target/glonax_${VERSION}_amd64/usr/local/bin
 cp ./target/release/glonax-netctl ./target/glonax_${VERSION}_amd64/usr/local/bin
-cp ./target/release/glonax-signetd ./target/glonax_${VERSION}_amd64/usr/local/bin
+cp ./target/release/glonax-traced ./target/glonax_${VERSION}_amd64/usr/local/bin
 
 # Set permissions
 chmod 755 ./target/glonax_${VERSION}_amd64/DEBIAN/postinst
