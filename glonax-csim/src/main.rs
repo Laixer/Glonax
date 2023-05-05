e965b5fa7201ea0be068dade259be88b8d738d4d
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-05-05 11:52:48 +0200

    Restructure simulator

--- glonax-csim/src/main.rs
+++ glonax-csim/src/main.rs
@@ -87,17 +87,47 @@ async fn main() -> anyhow::Result<()> {
     daemonize(&config).await
 }
 
-type PosLock = std::sync::Arc<std::sync::atomic::AtomicU32>;
+// struct EcuState([std::sync::atomic::AtomicI16; 4]);
 
-async fn listener(config: config::SimConfig, lock: PosLock, lock2: PosLock, lock3: PosLock) {
+struct EcuState {
+    power: [std::sync::atomic::AtomicI16; 4],
+    motion_lock: std::sync::atomic::AtomicBool,
+}
+
+impl EcuState {
+    fn new() -> Self {
+        Self {
+            power: [0.into(), 0.into(), 0.into(), 0.into()],
+            motion_lock: std::sync::atomic::AtomicBool::new(false),
+        }
+    }
+
+    fn lock(&self) {
+        self.power[0].store(0, std::sync::atomic::Ordering::Relaxed);
+        self.power[1].store(0, std::sync::atomic::Ordering::Relaxed);
+        self.power[2].store(0, std::sync::atomic::Ordering::Relaxed);
+        self.power[3].store(0, std::sync::atomic::Ordering::Relaxed);
+        self.motion_lock
+            .store(true, std::sync::atomic::Ordering::Relaxed);
+    }
+
+    fn unlock(&self) {
+        self.motion_lock
+            .store(false, std::sync::atomic::Ordering::Relaxed);
+    }
+
+    fn is_locked(&self) -> bool {
+        self.motion_lock.load(std::sync::atomic::Ordering::Relaxed)
+    }
+}
+
+async fn listener(config: config::SimConfig, state: std::sync::Arc<EcuState>) {
     use glonax::net::J1939Network;
 
     let network = J1939Network::new(config.interface.first().unwrap(), 0x4A).unwrap();
     let mut router = glonax::net::Router::new(network);
 
     let mut service = glonax::net::ActuatorService::new2(0x4A);
-
-    let mut locked = false;
 
     loop {
         if let Err(e) = router.listen().await {
@@ -106,93 +136,48 @@ async fn listener(config: config::SimConfig, lock: PosLock, lock2: PosLock, lock
 
         if let Some(message) = router.try_accept2(&mut service) {
             if let Some(motion_message) = message.1 {
-                log::trace!(
-                    "0x{:X?} » {}",
-                    router.frame_source().unwrap(),
-                    motion_message
-                );
-
-                locked = motion_message.locked;
+                if motion_message.locked {
+                    state.lock();
+                } else {
+                    state.unlock();
+                }
             }
             if let Some(actuator_message) = message.0 {
-                if locked {
+                if state.is_locked() {
                     continue;
                 }
-                log::trace!(
-                    "0x{:X?} » {}",
-                    router.frame_source().unwrap(),
-                    actuator_message
-                );
 
-                if let Some(boom_va) = actuator_message.actuators[0] {
-                    let position = lock.load(std::sync::atomic::Ordering::SeqCst);
-
-                    if boom_va.is_negative() {
-                        let mut adap = boom_va.abs() as u32 / 5_000;
-                        if adap > position {
-                            adap = position;
-                        }
-                        lock.store(position - adap, std::sync::atomic::Ordering::Relaxed);
-                    } else {
-                        let adap = boom_va.abs() as u32 / 5_000;
-                        lock.store(
-                            (position + adap).min(1832 - 1),
-                            std::sync::atomic::Ordering::Relaxed,
-                        );
-                    }
+                // FRAME
+                if let Some(value) = actuator_message.actuators[1] {
+                    state.power[0].store(value, std::sync::atomic::Ordering::Relaxed);
                 }
 
-                if let Some(frame_va) = actuator_message.actuators[1] {
-                    let position = lock3.load(std::sync::atomic::Ordering::SeqCst);
-
-                    if frame_va.is_negative() {
-                        let mut adap = frame_va.abs() as u32 / 5_000;
-                        if adap > position {
-                            adap = position;
-                        }
-                        lock3.store(position - adap, std::sync::atomic::Ordering::Relaxed);
-                    } else {
-                        let adap = frame_va.abs() as u32 / 5_000;
-
-                        if position + adap > 6280 {
-                            let position = (position + adap) - 6280;
-                            lock3.store(position, std::sync::atomic::Ordering::Relaxed)
-                        } else {
-                            lock3.store(position + adap, std::sync::atomic::Ordering::Relaxed);
-                        }
-                    }
+                // BOOM
+                if let Some(value) = actuator_message.actuators[0] {
+                    state.power[1].store(value, std::sync::atomic::Ordering::Relaxed);
                 }
 
-                if let Some(arm_va) = actuator_message.actuators[4] {
-                    let position = lock2.load(std::sync::atomic::Ordering::SeqCst);
+                // ARM
+                if let Some(value) = actuator_message.actuators[4] {
+                    state.power[2].store(value, std::sync::atomic::Ordering::Relaxed);
+                }
 
-                    if arm_va.is_positive() {
-                        let mut adap = arm_va.abs() as u32 / 5_000;
-                        if adap > position {
-                            adap = position;
-                        }
-                        lock2.store(
-                            (position - adap).max(685),
-                            std::sync::atomic::Ordering::Relaxed,
-                        );
-                    } else {
-                        let adap = arm_va.abs() as u32 / 5_000;
-                        lock2.store(
-                            (position + adap).min(2751),
-                            std::sync::atomic::Ordering::Relaxed,
-                        );
-                    }
+                // ATTACHMENT
+                if let Some(value) = actuator_message.actuators[5] {
+                    state.power[3].store(value, std::sync::atomic::Ordering::Relaxed);
                 }
             }
         }
     }
 }
 
-async fn signal_writer(config: config::SimConfig, lock: PosLock, lock2: PosLock, lock3: PosLock) {
+async fn signal_writer(config: config::SimConfig, state: std::sync::Arc<EcuState>) {
     use glonax::net::{
         EngineManagementSystem, EngineMessage, J1939Network, KueblerEncoderService, Routable,
     };
     use rand::Rng;
+
+    let mut rng = rand::rngs::OsRng::default();
 
     let neta = J1939Network::new(config.interface.first().unwrap(), 0x6A).unwrap();
     let netb = J1939Network::new(config.interface.first().unwrap(), 0x6B).unwrap();
@@ -200,40 +185,69 @@ async fn signal_writer(config: config::SimConfig, lock: PosLock, lock2: PosLock,
     let netd = J1939Network::new(config.interface.first().unwrap(), 0x6D).unwrap();
     let net0 = J1939Network::new(config.interface.first().unwrap(), 0x0).unwrap();
 
-    let mut rng = rand::rngs::OsRng::default();
-
     let mut encoder_a = KueblerEncoderService::new(0x6A);
     let mut encoder_b = KueblerEncoderService::new(0x6B);
     let mut encoder_c = KueblerEncoderService::new(0x6C);
     let mut encoder_d = KueblerEncoderService::new(0x6D);
     let engine_management_system = EngineManagementSystem::new(0x0);
 
-    let position_d = rng.gen_range(0..=3140);
+    encoder_a.position = rng.gen_range(0..=6280);
+    encoder_b.position = rng.gen_range(0..=1832 - 1);
+    encoder_c.position = rng.gen_range(685 + 1..=2751 - 1);
+    encoder_d.position = rng.gen_range(0..=3100);
 
     loop {
-        encoder_a.position = lock3.load(std::sync::atomic::Ordering::SeqCst);
-        encoder_a.speed = 0;
-        neta.send_vectored(&encoder_a.encode()).await.unwrap();
+        {
+            let value = state.power[0].load(std::sync::atomic::Ordering::SeqCst);
 
-        tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+            let fac = value / 2_500;
+            let position_0 = (encoder_a.position as i16 + fac).clamp(0, 6280);
 
-        encoder_b.position = lock.load(std::sync::atomic::Ordering::SeqCst);
-        encoder_b.speed = 0;
-        netb.send_vectored(&encoder_b.encode()).await.unwrap();
+            encoder_a.position = position_0 as u32;
+            encoder_a.speed = 0;
+            neta.send_vectored(&encoder_a.encode()).await.unwrap();
 
-        tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+            tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+        }
 
-        encoder_c.position = lock2.load(std::sync::atomic::Ordering::SeqCst);
-        encoder_c.speed = 0;
-        netc.send_vectored(&encoder_c.encode()).await.unwrap();
+        {
+            let value = state.power[1].load(std::sync::atomic::Ordering::SeqCst);
 
-        tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+            let fac = value / 5_000;
+            let position_0 = (encoder_b.position as i16 + fac).clamp(0, 1832 - 1);
 
-        encoder_d.position = position_d;
-        encoder_d.speed = 0;
-        netd.send_vectored(&encoder_d.encode()).await.unwrap();
+            encoder_b.position = position_0 as u32;
+            encoder_b.speed = 0;
+            netb.send_vectored(&encoder_b.encode()).await.unwrap();
 
-        tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+            tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+        }
+
+        {
+            let value = state.power[2].load(std::sync::atomic::Ordering::SeqCst);
+
+            let fac = value / 5_000;
+            let position_0 = (encoder_c.position as i16 + fac).clamp(685, 2751);
+
+            encoder_c.position = position_0 as u32;
+            encoder_c.speed = 0;
+            netc.send_vectored(&encoder_c.encode()).await.unwrap();
+
+            tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+        }
+
+        {
+            let value = state.power[3].load(std::sync::atomic::Ordering::SeqCst);
+
+            let fac = value / 5_000;
+            let position_0 = (encoder_d.position as i16 + fac).clamp(0, 3100);
+
+            encoder_d.position = position_0 as u32;
+            encoder_d.speed = 0;
+            netd.send_vectored(&encoder_d.encode()).await.unwrap();
+
+            tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+        }
 
         let mut engine_message = EngineMessage::new(0x0);
 
@@ -250,24 +264,14 @@ async fn signal_writer(config: config::SimConfig, lock: PosLock, lock2: PosLock,
 }
 
 async fn daemonize(config: &config::SimConfig) -> anyhow::Result<()> {
-    use rand::Rng;
-
     let runtime = glonax::RuntimeBuilder::from_config(config)?
         .with_shutdown()
         .build();
 
-    let mut rng = rand::rngs::OsRng::default();
+    let state = std::sync::Arc::new(EcuState::new());
 
-    let yi = rng.gen_range(0..=1832 - 1);
-    let yi2 = rng.gen_range(685 + 1..=2751 - 1);
-    let yi2x = rng.gen_range(0..=6280);
-
-    let l = std::sync::Arc::new(std::sync::atomic::AtomicU32::new(yi));
-    let l2 = std::sync::Arc::new(std::sync::atomic::AtomicU32::new(yi2));
-    let l3 = std::sync::Arc::new(std::sync::atomic::AtomicU32::new(yi2x));
-
-    runtime.spawn_background_task(listener(config.clone(), l.clone(), l2.clone(), l3.clone()));
-    runtime.spawn_background_task(signal_writer(config.clone(), l, l2, l3));
+    runtime.spawn_background_task(listener(config.clone(), state.clone()));
+    runtime.spawn_background_task(signal_writer(config.clone(), state));
 
     runtime.wait_for_shutdown().await;
 
