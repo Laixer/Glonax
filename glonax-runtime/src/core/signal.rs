16491653bdca59bb7441bbbfcb514edcce732dad
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-08-07 12:18:26 +0200

    Use generic transport channel

--- glonax-runtime/src/core/signal.rs
+++ glonax-runtime/src/core/signal.rs
@@ -1,3 +1,19 @@
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+
+const PROTO_METRIC_TEMPERATURE: u8 = 0x00;
+const PROTO_METRIC_ANGLE: u8 = 0x01;
+const PROTO_METRIC_SPEED: u8 = 0x02;
+const PROTO_METRIC_ALTITUDE: u8 = 0x03;
+const PROTO_METRIC_HEADING: u8 = 0x04;
+const PROTO_METRIC_RPM: u8 = 0x05;
+const PROTO_METRIC_ACCELERATION: u8 = 0x06;
+const PROTO_METRIC_PERCENT: u8 = 0x07;
+const PROTO_METRIC_COORDINATES: u8 = 0x08;
+const PROTO_METRIC_TIMESTAMP: u8 = 0x09;
+const PROTO_METRIC_POINT2D: u8 = 0x0A;
+const PROTO_METRIC_POINT3D: u8 = 0x0B;
+const PROTO_METRIC_COUNT: u8 = 0x0C;
+
 #[derive(Clone, Copy, Debug)]
 #[repr(C)]
 pub enum Metric {
@@ -17,7 +33,7 @@ pub enum Metric {
     Acceleration((f32, f32, f32)),
     /// Percentage.
     Percent(i32),
-    /// WS84 coordinates.
+    /// WGS 84 coordinates.
     Coordinates((f32, f32)),
     /// Timestamp in seconds.
     Timestamp(u64),
@@ -48,19 +64,6 @@ impl Signal {
             function: function.into(),
             metric,
         }
-    }
-
-    pub fn bytes(&self) -> &[u8] {
-        unsafe {
-            std::slice::from_raw_parts(self as *const _ as *const u8, std::mem::size_of::<Self>())
-        }
-    }
-
-    pub fn from(data: &[u8]) -> Self {
-        let (head, body, _tail) = unsafe { data.align_to::<Self>() };
-        assert_eq!(head.len(), 0);
-
-        body[0]
     }
 }
 
@@ -128,5 +131,116 @@ impl std::fmt::Display for Metric {
             }
             Metric::Count(value) => write!(f, "Count: {}", value),
         }
+    }
+}
+
+impl Signal {
+    // TODO: Move to transport
+    // TODO: Copy into bytes directly
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut buf = BytesMut::with_capacity(32);
+
+        buf.put_u32(self.address);
+        buf.put_u32(self.function);
+
+        match self.metric {
+            Metric::Temperature(value) => {
+                buf.put_u8(PROTO_METRIC_TEMPERATURE);
+                buf.put_f32(value);
+            }
+            Metric::Angle(value) => {
+                buf.put_u8(PROTO_METRIC_TEMPERATURE);
+                buf.put_f32(value);
+            }
+            Metric::Speed(value) => {
+                buf.put_u8(PROTO_METRIC_SPEED);
+                buf.put_f32(value);
+            }
+            Metric::Altitude(value) => {
+                buf.put_u8(PROTO_METRIC_ALTITUDE);
+                buf.put_f32(value);
+            }
+            Metric::Heading(value) => {
+                buf.put_u8(PROTO_METRIC_HEADING);
+                buf.put_f32(value);
+            }
+            Metric::Rpm(value) => {
+                buf.put_u8(PROTO_METRIC_RPM);
+                buf.put_i32(value);
+            }
+            Metric::Acceleration((value_x, value_y, value_z)) => {
+                buf.put_u8(PROTO_METRIC_ACCELERATION);
+                buf.put_f32(value_x);
+                buf.put_f32(value_y);
+                buf.put_f32(value_z);
+            }
+            Metric::Percent(value) => {
+                buf.put_u8(PROTO_METRIC_PERCENT);
+                buf.put_i32(value);
+            }
+            Metric::Coordinates((value_lat, value_long)) => {
+                buf.put_u8(PROTO_METRIC_COORDINATES);
+                buf.put_f32(value_lat);
+                buf.put_f32(value_long);
+            }
+            Metric::Timestamp(value) => {
+                buf.put_u8(PROTO_METRIC_TIMESTAMP);
+                buf.put_u64(value);
+            }
+            Metric::Point2D((value_x, value_y)) => {
+                buf.put_u8(PROTO_METRIC_POINT2D);
+                buf.put_f32(value_x);
+                buf.put_f32(value_y);
+            }
+            Metric::Point3D((value_x, value_y, value_z)) => {
+                buf.put_u8(PROTO_METRIC_POINT3D);
+                buf.put_f32(value_x);
+                buf.put_f32(value_y);
+                buf.put_f32(value_z);
+            }
+            Metric::Count(value) => {
+                buf.put_u8(PROTO_METRIC_COUNT);
+                buf.put_u64(value);
+            }
+        }
+
+        buf.to_vec()
+    }
+}
+
+// TODO: Move to transport
+impl TryFrom<&[u8]> for Signal {
+    type Error = ();
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let mut buf = Bytes::copy_from_slice(value);
+
+        let address = buf.get_u32();
+        let function = buf.get_u32();
+
+        let metric = match buf.get_u8() {
+            PROTO_METRIC_TEMPERATURE => Metric::Temperature(buf.get_f32()),
+            PROTO_METRIC_ANGLE => Metric::Angle(buf.get_f32()),
+            PROTO_METRIC_SPEED => Metric::Speed(buf.get_f32()),
+            PROTO_METRIC_ALTITUDE => Metric::Altitude(buf.get_f32()),
+            PROTO_METRIC_HEADING => Metric::Heading(buf.get_f32()),
+            PROTO_METRIC_RPM => Metric::Rpm(buf.get_i32()),
+            PROTO_METRIC_ACCELERATION => {
+                Metric::Acceleration((buf.get_f32(), buf.get_f32(), buf.get_f32()))
+            }
+            PROTO_METRIC_PERCENT => Metric::Percent(buf.get_i32()),
+            PROTO_METRIC_COORDINATES => Metric::Coordinates((buf.get_f32(), buf.get_f32())),
+            PROTO_METRIC_TIMESTAMP => Metric::Timestamp(buf.get_u64()),
+            PROTO_METRIC_POINT2D => Metric::Point2D((buf.get_f32(), buf.get_f32())),
+            PROTO_METRIC_POINT3D => Metric::Point3D((buf.get_f32(), buf.get_f32(), buf.get_f32())),
+            PROTO_METRIC_COUNT => Metric::Count(buf.get_u64()),
+            _ => return Err(()),
+        };
+
+        Ok(Self {
+            address,
+            function,
+            metric,
+        })
     }
 }
---
+++ glonax-runtime/src/transport.rs
@@ -0,0 +1,233 @@
+use bytes::{Buf, BufMut, BytesMut};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+
+use crate::core::{Motion, Signal};
+
+const PROTO_HEADER: [u8; 3] = [b'L', b'X', b'R'];
+const PROTO_VERSION: u8 = 0x01;
+
+const PROTO_MESSAGE_START: u8 = 0x10;
+const PROTO_MESSAGE_SHUTDOWN: u8 = 0x11;
+const PROTO_MESSAGE_MOTION: u8 = 0x20;
+const PROTO_MESSAGE_SIGNAL: u8 = 0x31;
+
+const MIN_BUFFER_SIZE: usize = PROTO_HEADER.len()
+    + std::mem::size_of::<u8>()
+    + std::mem::size_of::<u8>()
+    + std::mem::size_of::<u16>();
+
+pub enum Message {
+    Start(frame::Start),
+    Shutdown,
+    Motion(Motion),
+    Signal(Signal),
+}
+
+pub mod frame {
+    pub struct Start {
+        name: String,
+    }
+
+    impl Start {
+        pub fn new(name: String) -> Self {
+            Self { name }
+        }
+
+        pub fn name(&self) -> &str {
+            &self.name
+        }
+
+        pub fn to_bytes(&self) -> &[u8] {
+            self.name.as_bytes()
+        }
+    }
+}
+
+pub struct Protocol<T> {
+    inner: T,
+    buffer: BytesMut,
+}
+
+impl<T> Protocol<T> {
+    pub fn new(inner: T) -> Self {
+        Self {
+            inner,
+            buffer: BytesMut::with_capacity(2048),
+        }
+    }
+
+    // TOOD: Why not use the buffer directly?
+    fn build_frame<'a>(
+        &'a mut self,
+        buffer: &'a mut BytesMut,
+        message: u8,
+        payload: &[u8],
+    ) -> &'a [u8] {
+        buffer.put(&PROTO_HEADER[..]);
+        buffer.put_u8(PROTO_VERSION);
+        buffer.put_u8(message);
+        buffer.put_u16(payload.len() as u16);
+        buffer.put(&payload[..]);
+
+        &buffer[..]
+    }
+}
+
+impl<T: AsyncWrite + Unpin> Protocol<T> {
+    pub async fn write_frame0(&mut self, start: frame::Start) -> std::io::Result<()> {
+        let payload = start.to_bytes();
+
+        let mut buffer = BytesMut::with_capacity(MIN_BUFFER_SIZE + payload.len());
+
+        self.build_frame(&mut buffer, PROTO_MESSAGE_START, payload);
+
+        self.inner.write_all(&buffer[..]).await
+    }
+
+    pub async fn write_frame1(&mut self) -> std::io::Result<()> {
+        let mut buffer = BytesMut::with_capacity(MIN_BUFFER_SIZE);
+
+        self.build_frame(&mut buffer, PROTO_MESSAGE_SHUTDOWN, &[]);
+
+        self.inner.write_all(&buffer[..]).await
+    }
+
+    pub async fn write_frame5(&mut self, motion: Motion) -> std::io::Result<()> {
+        let payload = motion.to_bytes();
+
+        let mut buffer = BytesMut::with_capacity(MIN_BUFFER_SIZE + payload.len());
+
+        self.build_frame(&mut buffer, PROTO_MESSAGE_MOTION, &payload);
+
+        self.inner.write_all(&buffer[..]).await
+    }
+
+    pub async fn write_frame6(&mut self, signal: Signal) -> std::io::Result<()> {
+        let payload = signal.to_bytes();
+
+        let mut buffer = BytesMut::with_capacity(MIN_BUFFER_SIZE + payload.len());
+
+        self.build_frame(&mut buffer, PROTO_MESSAGE_SIGNAL, &payload);
+
+        self.inner.write_all(&buffer[..]).await
+    }
+
+    pub async fn write_all6(&mut self, signals: Vec<Signal>) -> std::io::Result<()> {
+        for signal in signals {
+            let payload = signal.to_bytes();
+
+            let mut buffer = BytesMut::with_capacity(MIN_BUFFER_SIZE + payload.len());
+
+            self.build_frame(&mut buffer, PROTO_MESSAGE_SIGNAL, &payload);
+
+            self.inner.write_all(&buffer[..]).await?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<T: AsyncRead + Unpin> Protocol<T> {
+    async fn read_at_least(&mut self, min_len: usize) -> std::io::Result<Option<usize>> {
+        let mut total_bytes_read = None;
+        while self.buffer.len() < min_len {
+            let bytes_read = self.inner.read_buf(&mut self.buffer).await?;
+            total_bytes_read = Some(bytes_read + total_bytes_read.unwrap_or(0));
+        }
+
+        Ok(total_bytes_read)
+    }
+
+    pub async fn read_frame(&mut self) -> std::io::Result<Message> {
+        loop {
+            let bytes_read = self.read_at_least(MIN_BUFFER_SIZE).await.unwrap();
+            if let Some(bytes_read) = bytes_read {
+                if bytes_read == 0 {
+                    log::warn!("Unexpected EOF 1");
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::UnexpectedEof,
+                        "EOF",
+                    ));
+                }
+            }
+
+            // Find header
+            for i in 0..(self.buffer.len() - PROTO_HEADER.len()) {
+                if self.buffer[i] == PROTO_HEADER[0] {
+                    if self.buffer[i + 1] == PROTO_HEADER[1] {
+                        if self.buffer[i + 2] == PROTO_HEADER[2] {
+                            self.buffer.advance(i + PROTO_HEADER.len());
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // Check protocol version
+            let version = self.buffer.get_u8();
+            if version != PROTO_VERSION {
+                log::warn!("Invalid version {}", version);
+                continue;
+            }
+
+            let message = self.buffer.get_u8();
+
+            let proto_length = self.buffer.get_u16() as usize;
+            if proto_length > 4096 {
+                log::warn!("Invalid proto length {}", proto_length);
+                continue;
+            }
+
+            let bytes_read = self.read_at_least(proto_length).await.unwrap();
+            if let Some(bytes_read) = bytes_read {
+                if bytes_read == 0 {
+                    log::warn!("Unexpected EOF 1");
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::UnexpectedEof,
+                        "EOF",
+                    ));
+                }
+            }
+
+            if message == PROTO_MESSAGE_START {
+                let payload = self.buffer.split_to(proto_length);
+
+                let mut session_name = String::new();
+
+                for c in payload {
+                    session_name.push(c as char);
+                }
+
+                return Ok(Message::Start(frame::Start::new(session_name)));
+            } else if message == PROTO_MESSAGE_SHUTDOWN {
+                return Ok(Message::Shutdown);
+            } else if message == PROTO_MESSAGE_MOTION {
+                let payload = self.buffer.split_to(proto_length);
+
+                match Motion::try_from(&payload[..]) {
+                    Ok(motion) => {
+                        return Ok(Message::Motion(motion));
+                    }
+                    Err(_) => {
+                        log::warn!("Invalid motion payload");
+                        continue;
+                    }
+                }
+            } else if message == PROTO_MESSAGE_SIGNAL {
+                let payload = self.buffer.split_to(proto_length);
+
+                match Signal::try_from(&payload[..]) {
+                    Ok(signal) => {
+                        return Ok(Message::Signal(signal));
+                    }
+                    Err(_) => {
+                        log::warn!("Invalid signal payload");
+                        continue;
+                    }
+                }
+            } else {
+                log::error!("Invalid message type: {}", message);
+            }
+        }
+    }
+}
