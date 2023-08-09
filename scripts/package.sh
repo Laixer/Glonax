1e92f0ac76d2f4bce26f885a0938d34edbc1c831
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-08-09 13:47:57 +0200

    Remove old deps

--- scripts/package.sh
+++ scripts/package.sh
@@ -36,14 +36,12 @@ cp ./contrib/deb/control ./target/glonax_${VERSION}_amd64/DEBIAN
 cp ./contrib/deb/postinst ./target/glonax_${VERSION}_amd64/DEBIAN
 cp ./contrib/udev/79-glonax.rules ./target/glonax_${VERSION}_amd64/etc/udev/rules.d
 cp ./contrib/systemd/glonax-ecud@.service ./target/glonax_${VERSION}_amd64/etc/systemd/system
-cp ./contrib/systemd/glonax-inputd.service ./target/glonax_${VERSION}_amd64/etc/systemd/system
 
 # Copy binaries
 cp ./target/release/glonax-csim ./target/glonax_${VERSION}_amd64/usr/local/bin
 cp ./target/release/glonax-ecud ./target/glonax_${VERSION}_amd64/usr/local/bin
-cp ./target/release/glonax-hostd ./target/glonax_${VERSION}_amd64/usr/local/bin
 cp ./target/release/glonax-gnssd ./target/glonax_${VERSION}_amd64/usr/local/bin
-cp ./target/release/glonax-inputd ./target/glonax_${VERSION}_amd64/usr/local/bin
+cp ./target/release/glonax-input ./target/glonax_${VERSION}_amd64/usr/local/bin
 cp ./target/release/glonax-netctl ./target/glonax_${VERSION}_amd64/usr/local/bin
 cp ./target/release/glonax-proxyd ./target/glonax_${VERSION}_amd64/usr/local/bin
 
