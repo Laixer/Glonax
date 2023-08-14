b46a12aed5700d6ffcf63e2bb892c8ff0564d9b3
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-08-14 12:36:41 +0200

    Overhall signals
    - Use system wide recognized signals

--- glonax-runtime/src/core/signal.rs
+++ glonax-runtime/src/core/signal.rs
@@ -1,119 +1,123 @@
 use bytes::{Buf, BufMut, Bytes, BytesMut};
 
-const PROTO_METRIC_TEMPERATURE: u8 = 0x00;
-const PROTO_METRIC_ANGLE: u8 = 0x01;
-const PROTO_METRIC_SPEED: u8 = 0x02;
-const PROTO_METRIC_ALTITUDE: u8 = 0x03;
-const PROTO_METRIC_HEADING: u8 = 0x04;
-const PROTO_METRIC_RPM: u8 = 0x05;
-const PROTO_METRIC_ACCELERATION: u8 = 0x06;
-const PROTO_METRIC_PERCENT: u8 = 0x07;
-const PROTO_METRIC_COORDINATES: u8 = 0x08;
-const PROTO_METRIC_TIMESTAMP: u8 = 0x09;
-const PROTO_METRIC_POINT2D: u8 = 0x0A;
-const PROTO_METRIC_POINT3D: u8 = 0x0B;
-const PROTO_METRIC_COUNT: u8 = 0x0C;
+const PROTO_METRIC_VMS_UPTIME: u16 = 0x0;
+const PROTO_METRIC_VMS_TIMESTAMP: u16 = 0x1;
+const PROTO_METRIC_VMS_MEMORY_USAGE: u16 = 0x2;
+const PROTO_METRIC_VMS_SWAP_USAGE: u16 = 0x3;
+const PROTO_METRIC_VMS_CPU_LOAD: u16 = 0x4;
+
+const PROTO_METRIC_GNSS_LATLONG: u16 = 0x14;
+const PROTO_METRIC_GNSS_ALTITUDE: u16 = 0x15;
+const PROTO_METRIC_GNSS_SPEED: u16 = 0x16;
+const PROTO_METRIC_GNSS_HEADING: u16 = 0x17;
+const PROTO_METRIC_GNSS_SATELLITES: u16 = 0x18;
+
+const PROTO_METRIC_ENCODER_ABS_ANGLE: u16 = 0x50;
+const PROTO_METRIC_ENCODER_RPM: u16 = 0x51;
+
+const PROTO_METRIC_ENGINE_DRIVER_DEMAND: u16 = 0x60;
+const PROTO_METRIC_ENGINE_ACTUAL_ENGINE: u16 = 0x61;
+const PROTO_METRIC_ENGINE_RPM: u16 = 0x62;
 
 #[derive(Clone, Copy, Debug)]
 #[repr(C)]
 pub enum Metric {
-    /// Temperature in degrees celcius.
-    Temperature(f32),
-    /// Angle in radians.
-    Angle(f32),
-    /// Speed in meters per second.
-    Speed(f32),
-    /// Altitude in meters.
-    Altitude(f32),
-    /// Heading in degrees.
-    Heading(f32),
-    /// Revolutions per minute.
-    Rpm(i32),
-    /// Acceleration in mg.
-    Acceleration((f32, f32, f32)),
-    /// Percentage.
-    Percent(i32),
-    /// WGS 84 coordinates.
-    Coordinates((f32, f32)),
-    /// Timestamp in seconds.
-    Timestamp(u64),
-    //// Point in 2D space.
-    Point2D((f32, f32)),
-    /// Point in 3D space.
-    Point3D((f32, f32, f32)),
-    /// Number of unspecificed units.
-    Count(u64),
+    /// VMS Uptime in seconds.
+    VmsUptime(u64),
+    /// VMS Timestamp in seconds.
+    VmsTimestamp(chrono::DateTime<chrono::Utc>),
+    /// VMS Memory usage in percent.
+    VmsMemoryUsage((u64, u64)),
+    /// VMS Swap usage in percent.
+    VmsSwapUsage(u64),
+    /// VMS CPU load.
+    VmsCpuLoad((f64, f64, f64)),
+
+    /// GNSS Latitude and Longitude.
+    GnssLatLong((f32, f32)),
+    /// GNSS Altitude in meters.
+    GnssAltitude(f32),
+    /// GNSS Speed in meters per second.
+    GnssSpeed(f32),
+    /// GNSS Heading in degrees.
+    GnssHeading(f32),
+    /// GNSS Satellites.
+    GnssSatellites(u8),
+
+    /// Encoder Absolute Angle in radians.
+    EncoderAbsAngle((u8, f32)),
+    /// Encoder RPM.
+    EncoderRpm((u8, u16)),
+
+    /// Engine Driver Demand in percent.
+    EngineDriverDemand(u8),
+    /// Engine Actual Engine in percent.
+    EngineActualEngine(u8),
+    /// Engine RPM.
+    EngineRpm(u16),
 }
 
 #[derive(Clone, Copy, Debug)]
 #[repr(C)]
 pub struct Signal {
-    /// Device address.
-    pub address: u32,
-    /// Device driver function.
-    pub function: u32,
     /// Signal metric.
     pub metric: Metric,
 }
 
 impl Signal {
     /// Create new signal.
-    pub fn new<I: Into<u32>>(address: I, function: I, metric: Metric) -> Self {
-        Self {
-            address: address.into(),
-            function: function.into(),
-            metric,
-        }
+    pub fn new(metric: Metric) -> Self {
+        Self { metric }
     }
 }
 
 impl std::fmt::Display for Signal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "0x{:02X?}:{:02X?} » {}",
-            self.address, self.function, self.metric
-        )
+        write!(f, "{}", self.metric)
     }
 }
 
 impl std::fmt::Display for Metric {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Metric::Temperature(value) => write!(f, "{:.1}°C", value),
-            Metric::Angle(value) => write!(
+            Metric::VmsUptime(value) => write!(f, "VMS Uptime: {} seconds", value),
+            Metric::VmsTimestamp(datetime) => {
+                write!(f, "VMS Timestamp: {}", datetime)
+            }
+            Metric::VmsMemoryUsage((memory_used, memory_total)) => {
+                write!(
+                    f,
+                    "VMS Memory usage: {:.2}GB / {:.2}GB",
+                    *memory_used as f64 / 1024.0 / 1024.0 / 1024.0,
+                    *memory_total as f64 / 1024.0 / 1024.0 / 1024.0
+                )
+            }
+            Metric::VmsSwapUsage(value) => write!(f, "VMS Swap usage: {}%", value),
+            Metric::VmsCpuLoad((value_1, value_5, value_15)) => write!(
                 f,
-                "{:>6.2}rad {:>6.2}°",
-                value,
-                crate::core::rad_to_deg(*value)
+                "VMS CPU load: {:.1}%, {:.1}%, {:.1}%",
+                value_1, value_5, value_15
             ),
-            Metric::Speed(value) => write!(f, "{:.2}m/s", value),
-            Metric::Altitude(value) => write!(f, "{:.1}m", value),
-            Metric::Heading(value) => write!(f, "{:.1}°", value),
-            Metric::Rpm(value) => write!(f, "{}rpm", value),
-            Metric::Acceleration((value_x, value_y, value_z)) => {
-                write!(f, "({:.2}, {:.2}, {:.2})", value_x, value_y, value_z)
+            Metric::GnssLatLong((value_lat, value_long)) => {
+                write!(f, "GNSS LatLong: ({:.5}, {:.5})", value_lat, value_long)
             }
-            Metric::Percent(value) => write!(f, "{:.1}%", value),
-            Metric::Coordinates((value_lat, value_long)) => {
-                write!(f, "({:.5}, {:.5})", value_lat, value_long)
+            Metric::GnssAltitude(value) => write!(f, "GNSS Altitude: {:.1}m", value),
+            Metric::GnssSpeed(value) => write!(f, "GNSS Speed: {:.1}m/s", value),
+            Metric::GnssHeading(value) => write!(f, "GNSS Heading: {:.1}°", value),
+            Metric::GnssSatellites(value) => write!(f, "GNSS Satellites: {}", value),
+            Metric::EncoderAbsAngle((node, value)) => {
+                write!(
+                    f,
+                    "Encoder 0x{:X?} Abs Angle: {:.2}rad {:.2}°",
+                    node,
+                    value,
+                    crate::core::rad_to_deg(*value)
+                )
             }
-            Metric::Timestamp(value) => {
-                use chrono::{DateTime, NaiveDateTime, Utc};
-
-                if let Some(naive_datetime) = NaiveDateTime::from_timestamp_opt(*value as i64, 0) {
-                    write!(f, "{}", DateTime::<Utc>::from_utc(naive_datetime, Utc))
-                } else {
-                    write!(f, "{:>+5}", value)
-                }
-            }
-            Metric::Point2D((value_x, value_y)) => {
-                write!(f, "({:.2}, {:.2})", value_x, value_y)
-            }
-            Metric::Point3D((value_x, value_y, value_z)) => {
-                write!(f, "({:.2}, {:.2}, {:.2})", value_x, value_y, value_z)
-            }
-            Metric::Count(value) => write!(f, "{}", value),
+            Metric::EncoderRpm((node, value)) => write!(f, "Encoder 0x{:X?} RPM: {}", node, value),
+            Metric::EngineDriverDemand(value) => write!(f, "Engine Driver Demand: {}%", value),
+            Metric::EngineActualEngine(value) => write!(f, "Engine Actual Engine: {}%", value),
+            Metric::EngineRpm(value) => write!(f, "Engine RPM: {}", value),
         }
     }
 }
@@ -124,67 +128,72 @@ impl Signal {
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut buf = BytesMut::with_capacity(32);
 
-        buf.put_u32(self.address);
-        buf.put_u32(self.function);
-
         match self.metric {
-            Metric::Temperature(value) => {
-                buf.put_u8(PROTO_METRIC_TEMPERATURE);
-                buf.put_f32(value);
+            Metric::VmsUptime(value) => {
+                buf.put_u16(PROTO_METRIC_VMS_UPTIME);
+                buf.put_u64(value);
             }
-            Metric::Angle(value) => {
-                buf.put_u8(PROTO_METRIC_ANGLE);
-                buf.put_f32(value);
+            Metric::VmsTimestamp(value_datetime) => {
+                buf.put_u16(PROTO_METRIC_VMS_TIMESTAMP);
+                buf.put_i64(value_datetime.timestamp());
             }
-            Metric::Speed(value) => {
-                buf.put_u8(PROTO_METRIC_SPEED);
-                buf.put_f32(value);
+            Metric::VmsMemoryUsage((value_used, value_total)) => {
+                buf.put_u16(PROTO_METRIC_VMS_MEMORY_USAGE);
+                buf.put_u64(value_used);
+                buf.put_u64(value_total);
             }
-            Metric::Altitude(value) => {
-                buf.put_u8(PROTO_METRIC_ALTITUDE);
-                buf.put_f32(value);
+            Metric::VmsSwapUsage(value) => {
+                buf.put_u16(PROTO_METRIC_VMS_SWAP_USAGE);
+                buf.put_u64(value);
             }
-            Metric::Heading(value) => {
-                buf.put_u8(PROTO_METRIC_HEADING);
-                buf.put_f32(value);
+            Metric::VmsCpuLoad((value_1, value_5, value_15)) => {
+                buf.put_u16(PROTO_METRIC_VMS_CPU_LOAD);
+                buf.put_f64(value_1);
+                buf.put_f64(value_5);
+                buf.put_f64(value_15);
             }
-            Metric::Rpm(value) => {
-                buf.put_u8(PROTO_METRIC_RPM);
-                buf.put_i32(value);
-            }
-            Metric::Acceleration((value_x, value_y, value_z)) => {
-                buf.put_u8(PROTO_METRIC_ACCELERATION);
-                buf.put_f32(value_x);
-                buf.put_f32(value_y);
-                buf.put_f32(value_z);
-            }
-            Metric::Percent(value) => {
-                buf.put_u8(PROTO_METRIC_PERCENT);
-                buf.put_i32(value);
-            }
-            Metric::Coordinates((value_lat, value_long)) => {
-                buf.put_u8(PROTO_METRIC_COORDINATES);
+            Metric::GnssLatLong((value_lat, value_long)) => {
+                buf.put_u16(PROTO_METRIC_GNSS_LATLONG);
                 buf.put_f32(value_lat);
                 buf.put_f32(value_long);
             }
-            Metric::Timestamp(value) => {
-                buf.put_u8(PROTO_METRIC_TIMESTAMP);
-                buf.put_u64(value);
+            Metric::GnssAltitude(value) => {
+                buf.put_u16(PROTO_METRIC_GNSS_ALTITUDE);
+                buf.put_f32(value);
             }
-            Metric::Point2D((value_x, value_y)) => {
-                buf.put_u8(PROTO_METRIC_POINT2D);
-                buf.put_f32(value_x);
-                buf.put_f32(value_y);
+            Metric::GnssSpeed(value) => {
+                buf.put_u16(PROTO_METRIC_GNSS_SPEED);
+                buf.put_f32(value);
             }
-            Metric::Point3D((value_x, value_y, value_z)) => {
-                buf.put_u8(PROTO_METRIC_POINT3D);
-                buf.put_f32(value_x);
-                buf.put_f32(value_y);
-                buf.put_f32(value_z);
+            Metric::GnssHeading(value) => {
+                buf.put_u16(PROTO_METRIC_GNSS_HEADING);
+                buf.put_f32(value);
             }
-            Metric::Count(value) => {
-                buf.put_u8(PROTO_METRIC_COUNT);
-                buf.put_u64(value);
+            Metric::GnssSatellites(value) => {
+                buf.put_u16(PROTO_METRIC_GNSS_SATELLITES);
+                buf.put_u8(value);
+            }
+            Metric::EncoderAbsAngle((node, value)) => {
+                buf.put_u16(PROTO_METRIC_ENCODER_ABS_ANGLE);
+                buf.put_u8(node);
+                buf.put_f32(value);
+            }
+            Metric::EncoderRpm((node, value)) => {
+                buf.put_u16(PROTO_METRIC_ENCODER_RPM);
+                buf.put_u8(node);
+                buf.put_u16(value);
+            }
+            Metric::EngineDriverDemand(value) => {
+                buf.put_u16(PROTO_METRIC_ENGINE_DRIVER_DEMAND);
+                buf.put_u8(value);
+            }
+            Metric::EngineActualEngine(value) => {
+                buf.put_u16(PROTO_METRIC_ENGINE_ACTUAL_ENGINE);
+                buf.put_u8(value);
+            }
+            Metric::EngineRpm(value) => {
+                buf.put_u16(PROTO_METRIC_ENGINE_RPM);
+                buf.put_u16(value);
             }
         }
 
@@ -199,32 +208,36 @@ impl TryFrom<&[u8]> for Signal {
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         let mut buf = Bytes::copy_from_slice(value);
 
-        let address = buf.get_u32();
-        let function = buf.get_u32();
+        let metric = match buf.get_u16() {
+            PROTO_METRIC_VMS_UPTIME => Metric::VmsUptime(buf.get_u64()),
+            PROTO_METRIC_VMS_TIMESTAMP => {
+                let datetime = chrono::DateTime::<chrono::Utc>::from_utc(
+                    chrono::NaiveDateTime::from_timestamp_opt(buf.get_i64(), 0).unwrap(),
+                    chrono::Utc,
+                );
 
-        let metric = match buf.get_u8() {
-            PROTO_METRIC_TEMPERATURE => Metric::Temperature(buf.get_f32()),
-            PROTO_METRIC_ANGLE => Metric::Angle(buf.get_f32()),
-            PROTO_METRIC_SPEED => Metric::Speed(buf.get_f32()),
-            PROTO_METRIC_ALTITUDE => Metric::Altitude(buf.get_f32()),
-            PROTO_METRIC_HEADING => Metric::Heading(buf.get_f32()),
-            PROTO_METRIC_RPM => Metric::Rpm(buf.get_i32()),
-            PROTO_METRIC_ACCELERATION => {
-                Metric::Acceleration((buf.get_f32(), buf.get_f32(), buf.get_f32()))
+                Metric::VmsTimestamp(datetime)
             }
-            PROTO_METRIC_PERCENT => Metric::Percent(buf.get_i32()),
-            PROTO_METRIC_COORDINATES => Metric::Coordinates((buf.get_f32(), buf.get_f32())),
-            PROTO_METRIC_TIMESTAMP => Metric::Timestamp(buf.get_u64()),
-            PROTO_METRIC_POINT2D => Metric::Point2D((buf.get_f32(), buf.get_f32())),
-            PROTO_METRIC_POINT3D => Metric::Point3D((buf.get_f32(), buf.get_f32(), buf.get_f32())),
-            PROTO_METRIC_COUNT => Metric::Count(buf.get_u64()),
+            PROTO_METRIC_VMS_MEMORY_USAGE => Metric::VmsMemoryUsage((buf.get_u64(), buf.get_u64())),
+            PROTO_METRIC_VMS_SWAP_USAGE => Metric::VmsSwapUsage(buf.get_u64()),
+            PROTO_METRIC_VMS_CPU_LOAD => {
+                Metric::VmsCpuLoad((buf.get_f64(), buf.get_f64(), buf.get_f64()))
+            }
+            PROTO_METRIC_GNSS_LATLONG => Metric::GnssLatLong((buf.get_f32(), buf.get_f32())),
+            PROTO_METRIC_GNSS_ALTITUDE => Metric::GnssAltitude(buf.get_f32()),
+            PROTO_METRIC_GNSS_SPEED => Metric::GnssSpeed(buf.get_f32()),
+            PROTO_METRIC_GNSS_HEADING => Metric::GnssHeading(buf.get_f32()),
+            PROTO_METRIC_GNSS_SATELLITES => Metric::GnssSatellites(buf.get_u8()),
+            PROTO_METRIC_ENCODER_ABS_ANGLE => {
+                Metric::EncoderAbsAngle((buf.get_u8(), buf.get_f32()))
+            }
+            PROTO_METRIC_ENCODER_RPM => Metric::EncoderRpm((buf.get_u8(), buf.get_u16())),
+            PROTO_METRIC_ENGINE_DRIVER_DEMAND => Metric::EngineDriverDemand(buf.get_u8()),
+            PROTO_METRIC_ENGINE_ACTUAL_ENGINE => Metric::EngineActualEngine(buf.get_u8()),
+            PROTO_METRIC_ENGINE_RPM => Metric::EngineRpm(buf.get_u16()),
             _ => return Err(()),
         };
 
-        Ok(Self {
-            address,
-            function,
-            metric,
-        })
+        Ok(Self { metric })
     }
 }
