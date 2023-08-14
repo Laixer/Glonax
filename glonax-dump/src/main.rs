4e55bd3f3c1fc452e20f8489da457ac834407b45
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-08-14 22:14:29 +0200

    Update FK WIP

--- glonax-dump/src/main.rs
+++ glonax-dump/src/main.rs
@@ -4,6 +4,8 @@
 // This software may be modified and distributed under the terms
 // of the included license.  See the LICENSE file for details.
 
+use std::collections::HashMap;
+
 use na::{Isometry, IsometryMatrix3, Rotation3, Translation3};
 use nalgebra as na;
 
@@ -29,22 +31,63 @@ impl Joint {
         }
     }
 
-    pub fn origin(mut self, origin_x: f32, origin_y: f32, origin_z: f32) -> Self {
+    pub fn origin_translation(mut self, origin_x: f32, origin_y: f32, origin_z: f32) -> Self {
         self.origin = na::IsometryMatrix3::from_parts(
             Translation3::new(origin_x, origin_y, origin_z),
             Rotation3::identity(),
         );
         self
     }
+
+    pub fn origin_rotation(mut self, origin_roll: f32, origin_pitch: f32, origin_yaw: f32) -> Self {
+        // self.origin = na::IsometryMatrix3::from_parts(
+        //     Translation3::new(origin_x, origin_y, origin_z),
+        //     Rotation3::identity(),
+        // );
+        self
+    }
+}
+
+enum DeviceType {
+    EncoderAbsoluteMultiTurn,
+    EncoderAbsoluteSingleTurn,
+}
+
+struct Device {
+    name: String,
+    id: u8,
+    ty: DeviceType,
+    options: HashMap<String, String>,
+}
+
+impl Device {
+    fn new(name: impl ToString, id: u8, ty: DeviceType) -> Self {
+        Self {
+            name: name.to_string(),
+            id,
+            ty,
+            options: HashMap::new(),
+        }
+    }
+}
+
+enum RobotType {
+    Excavator,
+    WheelLoader,
+    Dozer,
+    Grader,
+    Hauler,
+    Forestry,
 }
 
 struct Robot {
     instance: String, // TODO: Replace with UUID
     name: String,
     model: String,
+    ty: RobotType,
     joints: Vec<Joint>,
     chains: Vec<String>,
-    devices: Vec<String>,
+    devices: Vec<Device>,
     position_state: Vec<f64>,
 }
 
@@ -54,24 +97,32 @@ impl Robot {
             .iter()
             .find(|joint| joint.name == name.to_string())
     }
+
+    pub fn device_by_name(&self, name: impl ToString) -> Option<&Device> {
+        self.devices
+            .iter()
+            .find(|device| device.name == name.to_string())
+    }
 }
 
 struct RobotBuilder {
     instance: String, // TODO: Replace with UUID
     name: String,
     model: String,
+    ty: RobotType,
     joints: Vec<Joint>,
     chains: Vec<String>,
-    devices: Vec<String>,
+    devices: Vec<Device>,
     position_state: Vec<f64>,
 }
 
 impl RobotBuilder {
-    pub fn new(instance: impl ToString) -> Self {
+    pub fn new(instance: impl ToString, ty: RobotType) -> Self {
         Self {
             instance: instance.to_string(),
             name: String::new(),
             model: String::new(),
+            ty,
             joints: Vec::new(),
             chains: Vec::new(),
             devices: Vec::new(),
@@ -84,8 +135,8 @@ impl RobotBuilder {
         self
     }
 
-    pub fn model(mut self, model: String) -> Self {
-        self.model = model;
+    pub fn model(mut self, model: impl ToString) -> Self {
+        self.model = model.to_string();
         self
     }
 
@@ -99,7 +150,7 @@ impl RobotBuilder {
         self
     }
 
-    pub fn add_device(mut self, device: String) -> Self {
+    pub fn add_device(mut self, device: Device) -> Self {
         self.devices.push(device);
         self
     }
@@ -109,158 +160,167 @@ impl RobotBuilder {
             instance: self.instance,
             name: self.name,
             model: self.model,
+            ty: self.ty,
             joints: self.joints,
-            chains: Vec::new(),
-            devices: Vec::new(),
-            position_state: Vec::new(),
+            chains: self.chains,
+            devices: self.devices,
+            position_state: self.position_state,
         }
     }
 }
 
+trait EulerAngles {
+    fn from_roll(roll: f32) -> Self;
+    fn from_pitch(pitch: f32) -> Self;
+    fn from_yaw(pitch: f32) -> Self;
+}
+
+impl EulerAngles for Rotation3<f32> {
+    fn from_roll(roll: f32) -> Self {
+        Rotation3::from_euler_angles(roll, 0.0, 0.0)
+    }
+
+    fn from_pitch(pitch: f32) -> Self {
+        Rotation3::from_euler_angles(0.0, pitch, 0.0)
+    }
+
+    fn from_yaw(yaw: f32) -> Self {
+        Rotation3::from_euler_angles(0.0, 0.0, yaw)
+    }
+}
+
+#[derive(Debug, serde_derive::Deserialize)]
+struct AgentConfig {
+    host: String,
+}
+
+#[derive(Debug, serde_derive::Deserialize)]
+struct Config {
+    instance: String,
+    name: Option<String>,
+    r#type: String,
+    telemetry: Option<AgentConfig>,
+}
+
+fn locate_config_file(path: Option<&str>) -> Option<String> {
+    if let Some(path) = path {
+        if std::path::Path::new(path).exists() {
+            return Some(path.to_string());
+        }
+    }
+
+    if std::path::Path::new("glonax.conf").exists() {
+        return Some("glonax.conf".to_string());
+    }
+
+    if std::path::Path::new("/etc/glonax.conf").exists() {
+        return Some("/etc/glonax.conf".to_string());
+    }
+
+    None
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut client =
-        glonax::transport::Client::connect(&"localhost".to_string(), "glonax-dump".to_string())
-            .await?;
+    use std::io::Read;
 
-    let robot = RobotBuilder::new("36ebd674-21fa-4497-a712-eb2ee6b2cda1")
-        .name("gustav")
+    let config_file = "contrib/etc/glonax.conf";
+
+    let config_file = locate_config_file(Some(config_file)).expect("Config file not found");
+
+    let mut file = std::fs::File::open(config_file)?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let global_config: Config = toml::from_str(&contents)?;
+
+    let mut client = glonax::transport::Client::connect("localhost:30051", "glonax-dump").await?;
+
+    let robot = RobotBuilder::new(global_config.instance, RobotType::Excavator)
+        .model(global_config.r#type)
+        .name(global_config.name.unwrap_or("Unnamed".to_string()))
+        .add_device(Device::new(
+            "frame_encoder",
+            0x6A,
+            DeviceType::EncoderAbsoluteMultiTurn,
+        ))
+        .add_device(Device::new(
+            "boom_encoder",
+            0x6B,
+            DeviceType::EncoderAbsoluteSingleTurn,
+        ))
+        .add_device(Device::new(
+            "arm_encoder",
+            0x6C,
+            DeviceType::EncoderAbsoluteSingleTurn,
+        ))
+        .add_device(Device::new(
+            "attachment_encoder",
+            0x6D,
+            DeviceType::EncoderAbsoluteSingleTurn,
+        ))
         .add_joint(Joint::new("undercarriage", JointType::Fixed))
-        .add_joint(Joint::new("frame", JointType::Continuous).origin(0.0, 0.0, 1.295))
-        .add_joint(Joint::new("boom", JointType::Revolute).origin(0.16, 0.0, 0.595))
-        .add_joint(Joint::new("arm", JointType::Revolute).origin(6.0, 0.0, 0.0))
-        .add_joint(Joint::new("attachment", JointType::Revolute).origin(2.97, 0.0, 0.0))
-        .add_joint(Joint::new("end", JointType::Fixed))
+        .add_joint(Joint::new("frame", JointType::Continuous).origin_translation(0.0, 0.0, 1.295))
+        .add_joint(Joint::new("boom", JointType::Revolute).origin_translation(0.16, 0.0, 0.595))
+        .add_joint(Joint::new("arm", JointType::Revolute).origin_translation(6.0, 0.0, 0.0))
+        .add_joint(Joint::new("attachment", JointType::Revolute).origin_translation(2.97, 0.0, 0.0))
+        .add_joint(Joint::new("effector", JointType::Fixed).origin_translation(1.5, 0.0, 0.0))
         .build();
 
     let point = na::Point3::new(0.0, 0.0, 0.0);
 
-    let mut frame_yaw = 0.0; //std::f32::consts::FRAC_PI_2; //-0.523599; //-std::f64::consts::FRAC_PI_3;
-    let mut boom_pitch = 0.0; //-0.523599; //-std::f64::consts::FRAC_PI_3;
-    let mut arm_pitch = 0.0; //-0.523599; //-std::f64::consts::FRAC_PI_3;
-    let mut attachment_pitch = 0.0; //-0.523599; //-std::f64::consts::FRAC_PI_3;
+    let mut frame_yaw = 0.0;
+    let mut boom_pitch = 0.0;
+    let mut arm_pitch = 0.0;
+    let mut attachment_pitch = 0.0;
+
+    let frame_encoder = robot.device_by_name("frame_encoder").unwrap();
+    let boom_encoder = robot.device_by_name("boom_encoder").unwrap();
+    let arm_encoder = robot.device_by_name("arm_encoder").unwrap();
+    let attachment_encoder = robot.device_by_name("attachment_encoder").unwrap();
 
     let frame_joint = robot.joint_by_name("frame").unwrap();
     let boom_joint = robot.joint_by_name("boom").unwrap();
     let arm_joint = robot.joint_by_name("arm").unwrap();
     let attachment_joint = robot.joint_by_name("attachment").unwrap();
-
-    // let effector_point = (frame_joint.origin * Rotation3::from_euler_angles(0.0, 0.0, frame_yaw))
-    //     * (boom_joint.origin * Rotation3::from_euler_angles(0.0, boom_pitch, 0.0))
-    //     * (arm_joint.origin * Rotation3::from_euler_angles(0.0, arm_pitch, 0.0))
-    //     * (attachment_joint.origin * Rotation3::from_euler_angles(0.0, attachment_pitch, 0.0))
-    //     * point;
-
-    // println!(
-    //     "effector_point: [{:.2}, {:.2}, {:.2}]",
-    //     effector_point.x, effector_point.y, effector_point.z
-    // );
+    let effector_joint = robot.joint_by_name("effector").unwrap();
 
     while let Ok(signal) = client.recv_signal().await {
         match signal.metric {
             glonax::core::Metric::EncoderAbsAngle((node, value)) => match node {
-                0x6A => {
-                    // println!(
-                    //     "Frame Abs Angle: {:.2}rad {:.2}°",
-                    //     value,
-                    //     glonax::core::rad_to_deg(value)
-                    // );
-                    frame_yaw = value;
-
-                    let effector_point = (frame_joint.origin
-                        * Rotation3::from_euler_angles(0.0, 0.0, frame_yaw))
-                        * (boom_joint.origin * Rotation3::from_euler_angles(0.0, boom_pitch, 0.0))
-                        * (arm_joint.origin * Rotation3::from_euler_angles(0.0, arm_pitch, 0.0))
-                        * (attachment_joint.origin
-                            * Rotation3::from_euler_angles(0.0, attachment_pitch, 0.0))
-                        * point;
-
-                    // println!(
-                    //     "effector_point: [{:.2}, {:.2}, {:.2}]",
-                    //     effector_point.x, effector_point.y, effector_point.z
-                    // );
-                }
-                0x6B => {
-                    //     println!(
-                    //     "Boom Abs Angle: {:.2}rad {:.2}°",
-                    //     value,
-                    //     glonax::core::rad_to_deg(value)
-                    // );
-                    boom_pitch = value;
-
-                    let effector_point = (frame_joint.origin
-                        * Rotation3::from_euler_angles(0.0, 0.0, frame_yaw))
-                        * (boom_joint.origin * Rotation3::from_euler_angles(0.0, boom_pitch, 0.0))
-                        * (arm_joint.origin * Rotation3::from_euler_angles(0.0, arm_pitch, 0.0))
-                        * (attachment_joint.origin
-                            * Rotation3::from_euler_angles(0.0, attachment_pitch, 0.0))
-                        * point;
-
-                    // println!(
-                    //     "effector_point: [{:.2}, {:.2}, {:.2}]",
-                    //     effector_point.x, effector_point.y, effector_point.z
-                    // );
-                }
-                0x6C => {
-                    // println!(
-                    //     "Arm Abs Angle: {:.2}rad {:.2}°",
-                    //     value,
-                    //     glonax::core::rad_to_deg(value)
-                    // );
-
-                    arm_pitch = value;
-
-                    let effector_point = (frame_joint.origin
-                        * Rotation3::from_euler_angles(0.0, 0.0, frame_yaw))
-                        * (boom_joint.origin * Rotation3::from_euler_angles(0.0, boom_pitch, 0.0))
-                        * (arm_joint.origin * Rotation3::from_euler_angles(0.0, arm_pitch, 0.0))
-                        * (attachment_joint.origin
-                            * Rotation3::from_euler_angles(0.0, attachment_pitch, 0.0))
-                        * point;
-
-                    // println!(
-                    //     "effector_point: [{:.2}, {:.2}, {:.2}]",
-                    //     effector_point.x, effector_point.y, effector_point.z
-                    // );
-                }
-                0x6D => {
-                    // println!(
-                    //     "Attachment Abs Angle: {:.2}rad {:.2}°",
-                    //     value,
-                    //     glonax::core::rad_to_deg(value)
-                    // );
-
-                    attachment_pitch = value;
-
-                    let effector_point = (frame_joint.origin
-                        * Rotation3::from_euler_angles(0.0, 0.0, frame_yaw))
-                        * (boom_joint.origin * Rotation3::from_euler_angles(0.0, boom_pitch, 0.0))
-                        * (arm_joint.origin * Rotation3::from_euler_angles(0.0, arm_pitch, 0.0))
-                        * (attachment_joint.origin
-                            * Rotation3::from_euler_angles(0.0, attachment_pitch, 0.0))
-                        * point;
-
-                    // println!(
-                    //     "effector_point: [{:.2}, {:.2}, {:.2}]",
-                    //     effector_point.x, effector_point.y, effector_point.z
-                    // );
-                }
+                node if frame_encoder.id == node => frame_yaw = value,
+                node if boom_encoder.id == node => boom_pitch = value - 1.047,
+                node if arm_encoder.id == node => arm_pitch = value,
+                node if attachment_encoder.id == node => attachment_pitch = value - 0.962,
                 _ => {}
             },
             _ => {}
         }
 
+        let effector_point = (frame_joint.origin * Rotation3::from_yaw(frame_yaw))
+            * (boom_joint.origin * Rotation3::from_pitch(boom_pitch))
+            * (arm_joint.origin * Rotation3::from_pitch(arm_pitch))
+            * (attachment_joint.origin * Rotation3::from_pitch(attachment_pitch))
+            * effector_joint.origin
+            * point;
+
         println!(
-            "F Angle: {:5.2}rad {:5.2}°\tB Angle: {:5.2}rad {:5.2}°\tA Angle: {:5.2}rad {:5.2}°\tT Angle: {:5.2}rad {:5.2}°",
-            frame_yaw,
-            glonax::core::rad_to_deg(frame_yaw),
-            boom_pitch,
-            glonax::core::rad_to_deg(boom_pitch),
-            arm_pitch,
-            glonax::core::rad_to_deg(arm_pitch),
-            attachment_pitch,
-            glonax::core::rad_to_deg(attachment_pitch),
+            "effector_point: [{:.2}, {:.2}, {:.2}]",
+            effector_point.x, effector_point.y, effector_point.z
         );
+
+        // println!(
+        //     "F Angle: {:5.2}rad {:5.2}°\tB Angle: {:5.2}rad {:5.2}°\tA Angle: {:5.2}rad {:5.2}°\tT Angle: {:5.2}rad {:5.2}°",
+        //     frame_yaw,
+        //     glonax::core::rad_to_deg(frame_yaw),
+        //     boom_pitch,
+        //     glonax::core::rad_to_deg(boom_pitch),
+        //     arm_pitch,
+        //     glonax::core::rad_to_deg(arm_pitch),
+        //     attachment_pitch,
+        //     glonax::core::rad_to_deg(attachment_pitch),
+        // );
     }
 
     tokio::time::sleep(std::time::Duration::from_secs(5)).await;
