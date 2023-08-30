01b7748364554bad49baf8c486b741a79dce5a0c
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-08-30 12:10:11 +0200

    Fix inverse motion profile

--- glonax-dump/src/main.rs
+++ glonax-dump/src/main.rs
@@ -85,48 +85,15 @@ impl MotionProfile {
             let power = value * self.scale;
 
             if value > 0.0 {
-                (-(power.min(32_767.0 - self.offset)) - self.offset) as i32
+                (-power.max(-(32_767.0 - self.offset)) - self.offset) as i32
             } else {
-                (power.max(-(32_767.0 - self.offset)) + self.offset) as i32
+                (-power.min(32_767.0 - self.offset) + self.offset) as i32
             }
         } else {
             0
         }
     }
 }
-
-// class MotionProfile:
-//     def __init__(self, scale, offset, lower_bound, inverse):
-//         self.scale = scale
-//         self.offset = offset
-//         self.lower_bound = lower_bound
-//         self.inverse = inverse
-
-//     def power(self, value) -> int:
-//         if self.inverse:
-//             return int(self.proportional_power_inverse(value))
-//         return int(self.proportional_power(value))
-
-//     def proportional_power(self, value) -> int:
-//         if abs(value) > self.lower_bound:
-//             power = self.offset + min((abs(value) * self.scale), 32_767 - self.offset)
-//             if value < 0:
-//                 return -power
-//             else:
-//                 return power
-//         else:
-//             return 0
-
-//     def proportional_power_inverse(self, value) -> int:
-//         if abs(value) > self.lower_bound:
-//             power = value * self.scale
-
-//             if value > 0:
-//                 return max(-power, -(32_767 - self.offset)) - self.offset
-//             else:
-//                 return min(-power, 32_767 - self.offset) + self.offset
-//         else:
-//             return 0
 
 // TODO: move to core
 trait EulerAngles {
