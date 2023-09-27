a154a7a53cb4fc1d789908c69162f3a705a33dfe
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-09-27 11:06:12 +0200

    Load program from json file

--- glonax-dump/src/program.rs
+++ glonax-dump/src/program.rs
@@ -2,34 +2,13 @@ use std::{collections::VecDeque, path::Path};
 
 use glonax::geometry::Target;
 
-pub struct Program(VecDeque<Target>);
+pub type Program = VecDeque<Target>;
 
-impl Program {
-    pub fn load<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
-        let str = std::fs::read_to_string(path)?;
-        let targets: VecDeque<Target> = serde_json::from_str::<Vec<[f32; 6]>>(&str)?
-            .iter()
-            .map(|v| v.into())
-            .collect();
+pub fn from_file<P: AsRef<Path>>(path: P) -> std::io::Result<Program> {
+    let str = std::fs::read_to_string(path)?;
 
-        Ok(Self(targets))
-    }
-
-    pub fn iter(&self) -> impl Iterator<Item = &Target> {
-        self.0.iter()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
-
-    pub fn next(&mut self) -> Option<Target> {
-        self.0.pop_front()
-    }
-}
-
-impl FromIterator<Target> for Program {
-    fn from_iter<T: IntoIterator<Item = Target>>(iter: T) -> Self {
-        Self(iter.into_iter().collect())
-    }
+    Ok(serde_json::from_str::<Vec<[f32; 6]>>(&str)?
+        .iter()
+        .map(|v| v.into())
+        .collect())
 }
