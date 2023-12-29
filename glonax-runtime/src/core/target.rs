b47432159f104ef9bec45471d902a2d418fcb48b
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-12-29 15:44:47 +0100

    Add motion constraint to Target struct

--- glonax-runtime/src/core/target.rs
+++ glonax-runtime/src/core/target.rs
@@ -1,17 +1,43 @@
 use nalgebra::{Point3, UnitQuaternion};
 
 #[derive(Clone, Copy)]
+pub enum Constraint {
+    /// Unconstrained motion order.
+    Unconstrained = 0,
+    /// Motion control of the attachment is delayed until all other controllers have completed
+    DelayAttachment = 1,
+    /// Motion control will ignore the attachment.
+    StationaryAttachment = 2,
+    /// Linear priority.
+    LinearPriority = 20,
+    /// Lateral priority.
+    LateralPriority = 21,
+    /// Vertical priority.
+    VerticalPriority = 22,
+}
+
+#[derive(Clone, Copy)]
 pub struct Target {
     /// The point in space.
     pub point: Point3<f32>,
     /// The orientation of the target.
     pub orientation: UnitQuaternion<f32>,
+    /// The motion constraint.
+    pub constraint: Constraint,
 }
 
 impl Target {
     /// Construct a new target
-    pub fn new(point: Point3<f32>, orientation: UnitQuaternion<f32>) -> Self {
-        Self { point, orientation }
+    pub fn new(
+        point: Point3<f32>,
+        orientation: UnitQuaternion<f32>,
+        constraint: Constraint,
+    ) -> Self {
+        Self {
+            point,
+            orientation,
+            constraint,
+        }
     }
 
     /// Construct a new target from a point
@@ -19,6 +45,7 @@ impl Target {
         Self {
             point: Point3::new(x, y, z),
             orientation: UnitQuaternion::identity(),
+            constraint: Constraint::Unconstrained,
         }
     }
 }
@@ -61,6 +88,7 @@ impl From<(f32, f32, f32)> for Target {
         Self {
             point: Point3::new(x, y, z),
             orientation: UnitQuaternion::identity(),
+            constraint: Constraint::Unconstrained,
         }
     }
 }
@@ -70,6 +98,7 @@ impl From<(f32, f32, f32, f32, f32, f32)> for Target {
         Self {
             point: Point3::new(x, y, z),
             orientation: UnitQuaternion::from_euler_angles(roll, pitch, yaw),
+            constraint: Constraint::Unconstrained,
         }
     }
 }
@@ -79,6 +108,7 @@ impl From<[f32; 3]> for Target {
         Self {
             point: Point3::new(x, y, z),
             orientation: UnitQuaternion::identity(),
+            constraint: Constraint::Unconstrained,
         }
     }
 }
@@ -88,6 +118,7 @@ impl From<[f32; 6]> for Target {
         Self {
             point: Point3::new(x, y, z),
             orientation: UnitQuaternion::from_euler_angles(roll, pitch, yaw),
+            constraint: Constraint::Unconstrained,
         }
     }
 }
@@ -97,6 +128,7 @@ impl From<&[f32; 6]> for Target {
         Self {
             point: Point3::new(*x, *y, *z),
             orientation: UnitQuaternion::from_euler_angles(*roll, *pitch, *yaw),
+            constraint: Constraint::Unconstrained,
         }
     }
 }
@@ -106,6 +138,7 @@ impl From<Point3<f32>> for Target {
         Self {
             point,
             orientation: UnitQuaternion::identity(),
+            constraint: Constraint::Unconstrained,
         }
     }
 }
@@ -122,18 +155,30 @@ impl TryFrom<Vec<u8>> for Target {
         let orientation =
             UnitQuaternion::from_euler_angles(buf.get_f32(), buf.get_f32(), buf.get_f32());
 
-        Ok(Self { point, orientation })
+        Ok(Self {
+            point,
+            orientation,
+            constraint: match buf.get_u8() {
+                0 => Constraint::Unconstrained,
+                1 => Constraint::DelayAttachment,
+                2 => Constraint::StationaryAttachment,
+                20 => Constraint::LinearPriority,
+                21 => Constraint::LateralPriority,
+                22 => Constraint::VerticalPriority,
+                _ => return Err(()),
+            },
+        })
     }
 }
 
 impl crate::protocol::Packetize for Target {
     const MESSAGE_TYPE: u8 = 0x44;
-    const MESSAGE_SIZE: Option<usize> = Some(std::mem::size_of::<f32>() * 6);
+    const MESSAGE_SIZE: Option<usize> = Some((std::mem::size_of::<f32>() * 6) + 1);
 
     fn to_bytes(&self) -> Vec<u8> {
         use bytes::BufMut;
 
-        let mut buf = bytes::BytesMut::with_capacity(std::mem::size_of::<f32>() * 6);
+        let mut buf = bytes::BytesMut::with_capacity((std::mem::size_of::<f32>() * 6) + 1);
 
         buf.put_f32(self.point.coords[0]);
         buf.put_f32(self.point.coords[1]);
@@ -143,6 +188,8 @@ impl crate::protocol::Packetize for Target {
         buf.put_f32(roll);
         buf.put_f32(pitch);
         buf.put_f32(yaw);
+
+        buf.put_u8(self.constraint as u8);
 
         buf.to_vec()
     }
