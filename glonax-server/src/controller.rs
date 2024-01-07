b12e4375d067d6972d74a29a1af4bd4e3f6beea1
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-01-07 16:42:00 +0100

    Refactor motion control logic in Controller struct

--- glonax-server/src/controller.rs
+++ glonax-server/src/controller.rs
@@ -8,6 +8,11 @@ pub struct Controller {
     boom_profile: glonax::math::Linear,
     arm_profile: glonax::math::Linear,
     attachment_profile: glonax::math::Linear,
+    frame_stop: bool,
+    boom_stop: bool,
+    arm_stop: bool,
+    attachment_stop: bool,
+    stopall: bool,
 }
 
 impl<Cnf: Configurable> Component<Cnf> for Controller {
@@ -26,6 +31,11 @@ impl<Cnf: Configurable> Component<Cnf> for Controller {
             boom_profile,
             arm_profile,
             attachment_profile,
+            frame_stop: false,
+            boom_stop: false,
+            arm_stop: false,
+            attachment_stop: false,
+            stopall: false,
         }
     }
 
@@ -34,39 +44,74 @@ impl<Cnf: Configurable> Component<Cnf> for Controller {
         use glonax::core::Actuator;
         // use glonax::math::linear_motion;
 
-        let frame_error = *ctx.get(Actuator::Slew as u16).unwrap_or(&0.0);
-        let boom_error = *ctx.get(Actuator::Boom as u16).unwrap_or(&0.0);
-        let arm_error = *ctx.get(Actuator::Arm as u16).unwrap_or(&0.0);
-        let attachment_error = *ctx.get(Actuator::Attachment as u16).unwrap_or(&0.0);
+        let frame_error = ctx.get(Actuator::Slew as u16);
+        let boom_error = ctx.get(Actuator::Boom as u16);
+        let arm_error = ctx.get(Actuator::Arm as u16);
+        let attachment_error = ctx.get(Actuator::Attachment as u16);
 
-        log::debug!(
-            "Frame error: {}, boom error: {}, arm error: {}, attachment error: {}",
-            frame_error,
-            boom_error,
-            arm_error,
-            attachment_error
-        );
+        // let is_tri_arm_done = frame_error.is_none() && boom_error.is_none() && arm_error.is_none();
+        // let is_done = is_tri_arm_done && attachment_error.is_none();
 
-        let _frame_value = self.frame_profile.update(frame_error);
-        let _boom_value = self.boom_profile.update(boom_error);
-        let _arm_value = self.arm_profile.update(arm_error);
-        let _attachment_value = self.attachment_profile.update(attachment_error);
+        let mut motion = vec![];
 
-        // let is_tri_arm_done = frame_value.is_none() && boom_value.is_none() && arm_value.is_none();
-        // let is_done = is_tri_arm_done && attachment_value.is_none();
+        if let Some(error) = frame_error {
+            let value = self.frame_profile.update(*error);
+            log::debug!("Frame error: {}, value: {}", error, value);
 
-        // if is_done {
-        //     ctx.commit(glonax::core::Motion::StopAll);
+            motion.push((Actuator::Slew, value as i16));
+            self.frame_stop = false;
+        } else if !self.frame_stop {
+            log::debug!("Frame error: {}, value: {}", 0, 0);
 
-        //     // TODO: Keep the target, but set 'finished' to true
-        //     state.target = None;
-        // } else {
-        //     ctx.commit(glonax::core::Motion::from_iter(vec![
-        //         (Actuator::Slew, frame_value.unwrap_or(0)),
-        //         (Actuator::Boom, boom_value.unwrap_or(0)),
-        //         (Actuator::Arm, arm_value.unwrap_or(0)),
-        //         (Actuator::Attachment, attachment_value.unwrap_or(0)),
-        //     ]));
-        // }
+            motion.push((Actuator::Slew, 0));
+            self.frame_stop = true;
+        }
+
+        if let Some(error) = boom_error {
+            let value = self.boom_profile.update(*error);
+            log::debug!("Boom error: {}, value: {}", error, value);
+
+            motion.push((Actuator::Boom, value as i16));
+            self.boom_stop = false;
+        } else if !self.boom_stop {
+            log::debug!("Boom error: {}, value: {}", 0, 0);
+
+            motion.push((Actuator::Boom, 0));
+            self.boom_stop = true;
+        }
+
+        if let Some(error) = arm_error {
+            let value = self.arm_profile.update(*error);
+            log::debug!("Arm error: {}, value: {}", error, value);
+
+            motion.push((Actuator::Arm, value as i16));
+            self.arm_stop = false;
+        } else if !self.arm_stop {
+            log::debug!("Arm error: {}, value: {}", 0, 0);
+
+            motion.push((Actuator::Arm, 0));
+            self.arm_stop = true;
+        }
+
+        if let Some(error) = attachment_error {
+            let value = self.attachment_profile.update(*error);
+            log::debug!("Attachment error: {}, value: {}", error, value);
+
+            motion.push((Actuator::Attachment, value as i16));
+            self.attachment_stop = false;
+        } else if !self.attachment_stop {
+            log::debug!("Attachment error: {}, value: {}", 0, 0);
+
+            motion.push((Actuator::Attachment, 0));
+            self.attachment_stop = true;
+        }
+
+        if !motion.is_empty() {
+            ctx.commit(glonax::core::Motion::from_iter(motion));
+            self.stopall = false;
+        } else if !self.stopall {
+            ctx.commit(glonax::core::Motion::StopAll);
+            self.stopall = true;
+        }
     }
 }
