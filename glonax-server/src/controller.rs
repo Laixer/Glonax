fbf1b73f81d7cc98f8352aa9b613bab39596bdb9
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-01-07 19:07:37 +0100

    Refactor actuator state and update logic

--- glonax-server/src/controller.rs
+++ glonax-server/src/controller.rs
@@ -1,14 +1,68 @@
 use glonax::{
+    core::{Actuator, Motion},
     runtime::{Component, ComponentContext},
     Configurable, MachineState,
 };
 
+struct ActuatorMotionEvent {
+    actuator: Actuator,
+    error: f32,
+    value: i16,
+}
+
+struct ActuatorState {
+    profile: glonax::math::Linear,
+    actuator: Actuator,
+    stop: bool,
+}
+
+impl ActuatorState {
+    fn _new(actuator: Actuator, kp: f32, offset: f32, inverse: bool) -> Self {
+        Self {
+            profile: glonax::math::Linear::new(kp, offset, inverse),
+            actuator,
+            stop: false,
+        }
+    }
+
+    fn from_profile(actuator: Actuator, profile: glonax::math::Linear) -> Self {
+        Self {
+            profile,
+            actuator,
+            stop: false,
+        }
+    }
+
+    fn update(&mut self, error: Option<f32>) -> Option<ActuatorMotionEvent> {
+        if let Some(error) = error {
+            self.stop = false;
+
+            Some(ActuatorMotionEvent {
+                actuator: self.actuator,
+                error,
+                value: self.profile.update(error) as i16,
+            })
+        } else if !self.stop {
+            self.stop = true;
+
+            Some(ActuatorMotionEvent {
+                actuator: self.actuator,
+                error: 0.0,
+                value: Motion::POWER_NEUTRAL,
+            })
+        } else {
+            None
+        }
+    }
+}
+
 pub struct Controller {
-    frame_profile: glonax::math::Linear,
+    // frame_profile: glonax::math::Linear,
     boom_profile: glonax::math::Linear,
     arm_profile: glonax::math::Linear,
     attachment_profile: glonax::math::Linear,
-    frame_stop: bool,
+    frame_state: ActuatorState,
+    // frame_stop: bool,
     boom_stop: bool,
     arm_stop: bool,
     attachment_stop: bool,
@@ -26,12 +80,15 @@ impl<Cnf: Configurable> Component<Cnf> for Controller {
         let arm_profile = glonax::math::Linear::new(15_000.0, 12_000.0, true);
         let attachment_profile = glonax::math::Linear::new(15_000.0, 12_000.0, false);
 
+        let frame_state = ActuatorState::from_profile(Actuator::Slew, frame_profile);
+
         Self {
-            frame_profile,
+            // frame_profile,
             boom_profile,
             arm_profile,
             attachment_profile,
-            frame_stop: false,
+            frame_state,
+            // frame_stop: false,
             boom_stop: false,
             arm_stop: false,
             attachment_stop: false,
@@ -41,9 +98,6 @@ impl<Cnf: Configurable> Component<Cnf> for Controller {
 
     // TODO: If no errors are set, ignore the tick
     fn tick(&mut self, ctx: &mut ComponentContext, _state: &mut MachineState) {
-        use glonax::core::Actuator;
-        // use glonax::math::linear_motion;
-
         let frame_error = ctx.get(Actuator::Slew as u16);
         let boom_error = ctx.get(Actuator::Boom as u16);
         let arm_error = ctx.get(Actuator::Arm as u16);
@@ -54,18 +108,24 @@ impl<Cnf: Configurable> Component<Cnf> for Controller {
 
         let mut motion = vec![];
 
-        if let Some(error) = frame_error {
-            let value = self.frame_profile.update(*error);
-            log::debug!("Frame error: {}, value: {}", error, value);
+        if let Some(event) = self.frame_state.update(frame_error.copied()) {
+            log::debug!("{:?} error: {}, value: {}", event.actuator, event.error, event.value);
 
-            motion.push((Actuator::Slew, value as i16));
-            self.frame_stop = false;
-        } else if !self.frame_stop {
-            log::debug!("Frame error: {}, value: {}", 0, 0);
-
-            motion.push((Actuator::Slew, 0));
-            self.frame_stop = true;
+            motion.push((event.actuator, event.value));
         }
+
+        // if let Some(error) = frame_error {
+        //     let value = self.frame_profile.update(*error);
+        //     log::debug!("Frame error: {}, value: {}", error, value);
+
+        //     motion.push((Actuator::Slew, value as i16));
+        //     self.frame_stop = false;
+        // } else if !self.frame_stop {
+        //     log::debug!("Frame error: {}, value: {}", 0, Motion::POWER_NEUTRAL);
+
+        //     motion.push((Actuator::Slew, Motion::POWER_NEUTRAL));
+        //     self.frame_stop = true;
+        // }
 
         if let Some(error) = boom_error {
             let value = self.boom_profile.update(*error);
@@ -74,9 +134,9 @@ impl<Cnf: Configurable> Component<Cnf> for Controller {
             motion.push((Actuator::Boom, value as i16));
             self.boom_stop = false;
         } else if !self.boom_stop {
-            log::debug!("Boom error: {}, value: {}", 0, 0);
+            log::debug!("Boom error: {}, value: {}", 0, Motion::POWER_NEUTRAL);
 
-            motion.push((Actuator::Boom, 0));
+            motion.push((Actuator::Boom, Motion::POWER_NEUTRAL));
             self.boom_stop = true;
         }
 
@@ -87,9 +147,9 @@ impl<Cnf: Configurable> Component<Cnf> for Controller {
             motion.push((Actuator::Arm, value as i16));
             self.arm_stop = false;
         } else if !self.arm_stop {
-            log::debug!("Arm error: {}, value: {}", 0, 0);
+            log::debug!("Arm error: {}, value: {}", 0, Motion::POWER_NEUTRAL);
 
-            motion.push((Actuator::Arm, 0));
+            motion.push((Actuator::Arm, Motion::POWER_NEUTRAL));
             self.arm_stop = true;
         }
 
@@ -100,17 +160,17 @@ impl<Cnf: Configurable> Component<Cnf> for Controller {
             motion.push((Actuator::Attachment, value as i16));
             self.attachment_stop = false;
         } else if !self.attachment_stop {
-            log::debug!("Attachment error: {}, value: {}", 0, 0);
+            log::debug!("Attachment error: {}, value: {}", 0, Motion::POWER_NEUTRAL);
 
-            motion.push((Actuator::Attachment, 0));
+            motion.push((Actuator::Attachment, Motion::POWER_NEUTRAL));
             self.attachment_stop = true;
         }
 
         if !motion.is_empty() {
-            ctx.commit(glonax::core::Motion::from_iter(motion));
+            ctx.commit(Motion::from_iter(motion));
             self.stopall = false;
         } else if !self.stopall {
-            ctx.commit(glonax::core::Motion::StopAll);
+            ctx.commit(Motion::StopAll);
             self.stopall = true;
         }
     }
