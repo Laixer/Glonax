ff5afa8303491a709c8e231d6dabdf48891a3b94
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-01-24 14:28:03 +0100

    Fix rotation bug in SinusTest component

--- glonax-server/src/components/sintest.rs
+++ glonax-server/src/components/sintest.rs
@@ -17,13 +17,15 @@ impl<Cnf: Configurable> Component<Cnf> for SinusTest {
     }
 
     fn tick(&mut self, ctx: &mut ComponentContext, _state: &mut MachineState) {
-        let delta = ctx.delta().as_secs_f32();
+        let delta = ctx.delta();
 
         let actor = ctx.world.get_actor_by_name_mut(ROBOT_ACTOR_NAME).unwrap();
 
-        actor.add_relative_rotation(
-            "frame",
-            nalgebra::Rotation3::from_yaw(delta * 2.5_f32.to_radians()),
-        );
+        if delta.as_millis() < 100 {
+            actor.add_relative_rotation(
+                "frame",
+                nalgebra::Rotation3::from_yaw(delta.as_secs_f32() * 2.5_f32.to_radians()),
+            );
+        }
     }
 }
