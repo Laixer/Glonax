269f6c31d6b304a82389afd78d13fcc676153435
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-04-21 23:10:57 +0200

    Refactor glonax-runtime services and add Object enum and TryFrom implementation in mod.rs

--- glonax-runtime/src/core/mod.rs
+++ glonax-runtime/src/core/mod.rs
@@ -17,6 +17,34 @@ mod motion;
 mod status;
 mod target;
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Object {
+    Engine = 1,
+    Gnss = 2,
+    Host = 3,
+    Instance = 4,
+    Motion = 5,
+    Target = 6,
+    Status = 7,
+}
+
+impl TryFrom<u8> for Object {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(Self::Engine),
+            2 => Ok(Self::Gnss),
+            3 => Ok(Self::Host),
+            4 => Ok(Self::Instance),
+            5 => Ok(Self::Motion),
+            6 => Ok(Self::Target),
+            7 => Ok(Self::Status),
+            _ => Err(()),
+        }
+    }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq, serde_derive::Deserialize)]
 pub enum MachineType {
     /// Excavator.
