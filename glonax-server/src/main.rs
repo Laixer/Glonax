6806ef2121258281ee41232264f3041c87dc1c78
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-05-30 13:29:49 +0200

    Refactor code to improve modularity and update dependencies

--- glonax-server/src/main.rs
+++ glonax-server/src/main.rs
@@ -153,31 +153,32 @@ async fn run(config: config::Config) -> anyhow::Result<()> {
     // The entire pipeline execution should be moved to a MCU
     //
 
-    let mut ctx = glonax::runtime::ComponentContext::default();
+    // let mut ctx = glonax::runtime::ComponentContext::default();
 
-    world::construct(&mut ctx);
+    // world::construct(&mut ctx);
 
-    let mut pipe = service::ComponentExecutor::new(ctx);
+    // let mut pipe = service::ComponentExecutor::new(ctx);
 
-    pipe.add_init_component::<glonax::components::Acquisition>();
+    // pipe.add_init_component::<glonax::components::Acquisition>();
 
-    // if config.is_simulation {
-    //     pipe.add_component_default::<glonax::components::EncoderSimulator>();
-    //     pipe.add_component_default::<glonax::components::EngineSimulator>();
+    // // if config.is_simulation {
+    // //     pipe.add_component_default::<glonax::components::EncoderSimulator>();
+    // //     pipe.add_component_default::<glonax::components::EngineSimulator>();
+    // // }
+
+    // pipe.add_component_default::<components::Perception>();
+
+    // if config.mode == config::OperationMode::Autonomous {
+    //     pipe.add_component_default::<components::Planner>();
     // }
 
-    pipe.add_component_default::<components::Perception>();
+    // pipe.add_component_default::<components::Controller>(); // TODO: Rename to something more specific
 
-    if config.mode == config::OperationMode::Autonomous {
-        pipe.add_component_default::<components::Planner>();
-    }
+    // pipe.add_post_component::<glonax::components::CommitComponent>();
+    // pipe.add_post_component::<glonax::components::SignalComponent>();
 
-    pipe.add_component_default::<components::Controller>(); // TODO: Rename to something more specific
-
-    pipe.add_post_component::<glonax::components::CommitComponent>();
-    pipe.add_post_component::<glonax::components::SignalComponent>();
-
-    runtime.run_interval(pipe, SERVICE_PIPELINE_INTERVAL).await;
+    // runtime.run_interval(pipe, SERVICE_PIPELINE_INTERVAL).await;
+    runtime.wait_for_shutdown().await;
 
     log::info!("Waiting for shutdown");
 
