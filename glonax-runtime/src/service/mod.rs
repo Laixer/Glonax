53252747a8c76e1eae4156a38c5e67eb2b2b49db
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-07-01 21:17:12 +0200

    chore: Remove glonax-serial dependency and unused code

--- glonax-runtime/src/service/mod.rs
+++ glonax-runtime/src/service/mod.rs
@@ -1,11 +1,7 @@
 pub use authority::{NetworkAuthority, NetworkConfig};
 pub use director::Director;
-// pub use gnss::{Gnss, GnssConfig};
-// pub use host::Host;
 pub use tcp_server::{TcpServer, TcpServerConfig, UnixServerConfig};
 
 mod authority;
 mod director;
-// mod gnss;
-// mod host;
 mod tcp_server;
